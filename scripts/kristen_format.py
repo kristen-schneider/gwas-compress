19503e89c60168fc098f618c5d25c00955cf2f1a
kristen-schneider <kristen.schneider@colorado.edu>
2021-03-12 11:47:18 -0700

    breaking kristen_fortmat.py into two scripts to simplify the funnel format

--- scripts/kristen_format.py
+++ scripts/kristen_format.py
@@ -55,6 +55,16 @@ def get_header(f, delimeter):
         header = f_open.readline().rstrip().split(delimeter)
     return header
 
+def get_chr_format(f, delimeter):
+    '''
+    looking to see how the first column (chr) is formatted
+    
+    INPUTS
+    f = path to uncompressed input file
+    
+    '''    
+
+
  
 def make_blocks(f, block_size):
     '''
@@ -90,168 +100,3 @@ def make_blocks(f, block_size):
     return all_blocks
             
 
-def ks_ds_list_of_cols(num_columns, blocks, delimeter): 
-    '''
-    stores data in lists of cols
-
-    INPUTS
-    num_columns: number of columns (usually len(header))
-    blocks: list of strings, where each string is all lines in a block (e.g. '1\t100\t500\tA...\n1\t501\t600\G...'
-    delimeter: delimeter of file (e.g. tab, space, comma)
-    OUTPUTS
-    list of cols:
-        [[col1, col2, ..., coln],[col1, col2, ..., coln], ..., [col1, col2, ..., coln]]
-        
-        *specifically...*
-        list_cols = [block1, block2, ..., blockn]
-            block1 = [col1, col2, ..., colm]    
-    
-    '''
-    all_blocks = []
-    for b in blocks:
-        list_cols = [''] * num_columns
-        rows = b.split('\n')
-        for r in rows:
-            rows_list = r.split(delimeter)
-            for rl in range(len(rows_list)):
-                data = rows_list[rl] + delimeter
-                list_cols[rl] += data
-        all_blocks.append(list_cols) 
-    for b in all_blocks: print(b)
-    return all_blocks
-
-def ks_ds_dict_cols_blocks(header, blocks, delimeter):
-    '''
-    stores data in dictionary where keys are column names and values are list of blocks
-
-    INPUTS
-    blocks: list of strings, where each string is all lines in a block (e.g. '1\t100\t500\tA...\n1\t501\t600\G...'
-    OUTPUTS
-    dictionary of cols: blocks:
-        [col1:[block1, block2, ..., blockn],
-         col2:[block1, block2, ..., blockn], 
-            ...
-         colm:[block1, block2, ..., blockn]]
-    *col1 here might be "chromosome"*
-       
-    '''
-    
-    final_dict = {col: ['' for i in range(len(blocks))] for col in header}
-    for b in range(len(blocks)):
-        rows = blocks[b].split('\n')
-        for r in rows:
-            rows_list = r.split(delimeter)
-            for rl in range(len(rows_list)): 
-                final_dict[header[rl]][b] += (rows_list[rl] + '\t')
-                #final_dict[header[rl]][b].append(rows_list[rl])
-    for d in final_dict: print(d, final_dict[d])
-    return final_dict    
-
-def ks_ds_dict_blocks_cols(num_cols, blocks, delimeter):        
-    '''
-    stores data in lists of cols data structure
-
-    INPUTS
-    blocks: list of strings, where each string is all lines in a block (e.g. '1\t100\t500\tA...\n1\t501\t600\G...'
-    OUTPUTS
-    dictionary of blocks: cols:
-        [block1: [col1, col2, ..., colm],
-         block2: [col1, col2, ..., colm], 
-            ...
-         blockn: [col1, col2, ..., colm]]
-    
-    '''
-    header = ['chrm', 'pos', 'start', 'end', 'base']
-    final_dict = {block_num: ['' for i in range(num_cols)] for block_num in range(len(blocks))}
-    for b in range(len(blocks)):
-        rows = blocks[b].split('\n')
-        for r in rows:
-            rows_list = r.split(delimeter)
-            for rl in range(len(rows_list)):
-                final_dict[b][rl] += (rows_list[rl] + '\t')
-                #final_dict[b][rl].append(rows_list[rl])
-    for d in final_dict: print(d, final_dict[d])
-    return final_dict
- 
-
-def write_ds1(ds1):
-    '''
-    take take each element from ds1, convert to bytes, compress, and write to file
-
-    INPUTS
-    ds1: list of cols:
-        [[col1, col2, ..., coln],[col1, col2, ..., coln], ..., [col1, col2, ..., coln]]
-        
-        *specifically...*
-        list_cols = [block1, block2, ..., blockn]
-            block1 = [col1, col2, ..., colm]  
-    OUTPUTS
-    none. writes to out_file.
-
-    '''
-    # open new gz file
-    out_file = open('/Users/kristen/Desktop/compression_sandbox/toy_data/ds1.tsv.gz', 'ab')
-    out_file.truncate(0) 
-    # write to gz file
-    for b in ds1:
-        for col in b:
-            bytes_data = bytes(col, 'utf-8')
-            compressed_data = gzip.compress(bytes_data)
-            out_file.write(compressed_data)
-    out_file.close()
-
-def write_ds2(ds2):
-    '''
-    take each element from ds2, convert to bytes, compress, and write to file
-
-    INPUTS
-    dictionary of cols: blocks:
-        [col1:[block1, block2, ..., blockn],
-         col2:[block1, block2, ..., blockn], 
-            ...
-         colm:[block1, block2, ..., blockn]]
-    *col1 here might be "chromosome"*
-    OUTPUTS
-    none. writes to out_file.
-
-    '''
-    # open new gz file
-    out_file = open('/Users/kristen/Desktop/compression_sandbox/toy_data/ds2.tsv.gz', 'ab')
-    out_file.truncate(0)
-    # write to gz file
-    for c in ds2:
-        for b in ds2[c]:
-            bytes_data = bytes(b, 'utf-8')
-            compressed_data = gzip.compress(bytes_data)
-            out_file.write(compressed_data)
-    out_file.close()
-
-def write_ds3(ds3):
-    '''
-    take each element from ds3, convert to bytes, compress, and write to file
-
-    INPUTS
-    dictionary of blocks: cols:
-        [block1: [col1, col2, ..., colm],
-         block2: [col1, col2, ..., colm], 
-            ...
-         blockn: [col1, col2, ..., colm]]
-    OUTPUTS
-    none. writes to out_file.
-
-    '''
-    # open new gz file
-    out_file = open('/Users/kristen/Desktop/compression_sandbox/toy_data/ds3.tsv.gz', 'ab')
-    out_file.truncate(0)
-    # write to gz file
-    for b in ds3:
-        for col in ds3[b]:
-            bytes_data = bytes(col, 'utf-8')
-            compressed_data = gzip.compress(bytes_data)
-            out_file.write(compressed_data)
-    out_file.close()
-
-
-    
-if __name__ == '__main__':
-    main()
