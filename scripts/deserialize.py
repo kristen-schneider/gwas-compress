93af831c764f31777acec69f33fc1ae1c9ad755c
kristen-schneider <kristen.schneider@colorado.edu>
2021-03-15 12:49:03 -0600

    split deserialize into two functions, one which just handles a single bitstring to deserialize and one which can break a decompressed bitstring into many bitstrings to call deserialization

--- scripts/deserialize.py
+++ scripts/deserialize.py
@@ -1,51 +1,55 @@
-def deserialize_data(dc_bitstring, lengths_of_bitstrings, val_types_of_bitstrings, num_bytes_list):
+def deserialize_list_bitstrings(dc_bitstring, num_columns, lengths_of_bitstrings, val_types_of_bitstrings, num_bytes_list):
     '''
-    take decompressed bitstring (same as serilized bitstring) and deserializes it.
+    take decompressed bitstring and break it into columns of decompressed bitstrings. call deserialize_data on each individual decompressed bitstring.
 
     INPUTS
     dc_bitstring: decompressed bitstring
     lengths_of_bitstrings: list of lengths to separate bitstrings by column and deserialize (lengths are of dc_bitstrings).
     val_types_of_bitstrings: list of integers indicating what data type each column holds (e.g. [0,1,0,0,1]
     num_bytes: number of bytes that each character takes up (for now--same across all columns)
-    
-    OUTPUTS
-    deserialized bitstring (same as original list)
-    
-    '''
-    print(dc_bitstring)
-    ds_list = []
-    
-    offset = 0 
-    
-    for curr_i in range(len(lengths_of_bitstrings)):
-        # work with one bitstring segement at a time to deserialize
-        curr_len_bitstring = lengths_of_bitstrings[curr_i]
-        curr_val_type = val_types_of_bitstrings[curr_i]
-        curr_ds_bitstring = []
-        num_bytes = num_bytes_list[curr_i]
-        
-        for i in range(num_bytes):
-            i += offset
-            # input values are integers
-            if curr_val_type == 0:
-                curr_bytes = dc_bitstring[i*num_bytes:i*num_bytes+num_bytes]
-                curr_ds_value = int.from_bytes(curr_bytes, byteorder='big', signed=False)
-               
-            # input values are strings 
-            elif curr_val_type == 1:
-                curr_bytes = dc_bitstring[i]
-                print(curr_bytes)
-                curr_ds_value = chr(curr_bytes)
-            
-            # input values are floats--STILL WORKING ON
-            elif curr_val_type == 2:
-                curr_bytes = 0
-                curr_ds_value = 0                
-            
-            curr_ds_bitstring.append(curr_ds_value)
-        
-        offset+=curr_len_bitstring
-        ds_list.append(curr_ds_bitstring)
-    return ds_list
 
+    OUTPUTS
+    list_of_ds_bitstrings: list of deserialized bitstrings
+
+    '''
+
+    ds_bitstring_final = []
+    offset = 0
+    
+    for curr_i in range(num_columns):
+        # break full dc_bitstring into column-specific bitstrings
+        curr_length = lengths_of_bitstrings[curr_i]
+        curr_val_type = val_types_of_bitstrings[curr_i]
+        curr_num_bytes = num_bytes_list[curr_i]
+        curr_dc_bitstring = dc_bitstring[offset:offset+curr_length] 
+        offset+= curr_length    
+        # deserialize a single bitstring at a time
+        curr_ds_bitstring_list = deserialize_data(curr_dc_bitstring, curr_val_type, curr_num_bytes)        
+        ds_bitstring_final.append(curr_ds_bitstring_list)
+
+    return ds_bitstring_final 
+
+def deserialize_data(dc_bitstring, val_type, num_bytes):
+    curr_ds_bitstring = []
+     
+    for i in range(num_bytes):
+        # input values are integers
+        if val_type == 0:
+            curr_bytes = dc_bitstring[i*num_bytes:i*num_bytes+num_bytes]
+            curr_ds_value = int.from_bytes(curr_bytes, byteorder='big', signed=False)
+
+        # input values are strings 
+        elif val_type == 1:
+            curr_bytes = dc_bitstring[i]
+            curr_ds_value = chr(curr_bytes)
+
+        # input values are floats--STILL WORKING ON
+        elif val_type == 2:
+            curr_bytes = 0
+            curr_ds_value = 0
+
+
+        curr_ds_bitstring.append(curr_ds_value)
+    
+    return curr_ds_bitstring 
 
--- scripts/test_serialize_to_deserialize.py
+++ scripts/test_serialize_to_deserialize.py
@@ -9,8 +9,11 @@ import unittest
 # Testing serialization --> compression --> decompression --> deserialization
 class TestSerializationToDeserialization(unittest.TestCase):
     
-    # for reproducibility we choose mtime = 0 for unittests
-    mtime = 0
+    num_columns = 5
+    mtime = 0   # for reproducibility we choose mtime = 0 for unittests
+    int_type = 0
+    str_type = 1
+    float_type = 2
     num_bytes_int = 5
     num_bytes_str = 5
     num_bytes_float = 8
@@ -19,13 +22,13 @@ class TestSerializationToDeserialization(unittest.TestCase):
     A_s = b'\x00\x00\x00\x00\x01\x00\x00\x00\x00\x01\x00\x00\x00\x00\x01\x00\x00\x00\x00\x01\x00\x00\x00\x00\x01'
     A_c = b'\x1f\x8b\x08\x00\x00\x00\x00\x00\x02\xffc````d\xc0B\x00\x00s \xa8\xa5\x19\x00\x00\x00'
     A_dc = b'\x00\x00\x00\x00\x01\x00\x00\x00\x00\x01\x00\x00\x00\x00\x01\x00\x00\x00\x00\x01\x00\x00\x00\x00\x01'
-    A_ds = [[1,1,1,1,1]]
+    A_ds = [1,1,1,1,1]
     
     B = ['A', 'C', 'T', 'G', 'A']
     B_s = b'ACTGA'
     B_c = b'\x1f\x8b\x08\x00\x00\x00\x00\x00\x02\xffst\x0eqw\x04\x00\x9f_F\xff\x05\x00\x00\x00'
     B_dc = b'ACTGA'
-    B_ds = [['A', 'C', 'T', 'G', 'A']] 
+    B_ds = ['A', 'C', 'T', 'G', 'A']
     
     AB_dc = A_dc+B_dc
     AB_ds = [A, B]
@@ -51,11 +54,15 @@ class TestSerializationToDeserialization(unittest.TestCase):
         self.assertEqual(decompress.decompress_data(compress.compress_data(serialize.serialize_data(self.B, self.num_bytes_str), self.mtime)), self.B_dc)
     
     def test_deserialize(self):
-        self.assertEqual(deserialize.deserialize_data(self.A_dc, [len(self.A_dc)], [0], [self.num_bytes_int]), self.A_ds)
-        self.assertEqual(deserialize.deserialize_data(decompress.decompress_data(compress.compress_data(serialize.serialize_data(self.A, self.num_bytes_int), self.mtime)), [len(self.A)], [0], [self.num_bytes_int]), self.A_ds)
-        self.assertEqual(deserialize.deserialize_data(self.B_dc, [len(self.B_dc)], [1], [self.num_bytes_str]), self.B_ds)
-        self.assertEqual(deserialize.deserialize_data(decompress.decompress_data(compress.compress_data(serialize.serialize_data(self.B, self.num_bytes_str), self.mtime)), [len(self.B)], [1], [self.num_bytes_str]), self.B_ds)
-        self.assertEqual(deserialize.deserialize_data(self.AB_dc, [len(self.A_dc), len(self.B_dc)], [0,1], [self.num_bytes_int, self.num_bytes_str]), self.AB_ds)
+        # deserialize_data(dc_bitstring, val_type, num_bytes)
+        self.assertEqual(deserialize.deserialize_data(self.A_dc, self.int_type, self.num_bytes_int), self.A_ds)
+        self.assertEqual(deserialize.deserialize_data(decompress.decompress_data(compress.compress_data(serialize.serialize_data(self.A, self.num_bytes_int), self.mtime)), self.int_type, self.num_bytes_int), self.A_ds)
+        self.assertEqual(deserialize.deserialize_data(self.B_dc, self.str_type, self.num_bytes_str), self.B_ds)
+        self.assertEqual(deserialize.deserialize_data(decompress.decompress_data(compress.compress_data(serialize.serialize_data(self.B, self.num_bytes_str), self.mtime)), self.str_type, self.num_bytes_str), self.B_ds)
+        # deserialize_list_bitstrings(dc_bitstring, num_columns, lengths_of_bitstrings, val_types_of_bitstrings, num_bytes_list)
+        self.assertEqual(deserialize.deserialize_list_bitstrings(self.AB_dc, 2, [len(self.A_dc), len(self.B_dc)], [self.int_type, self.str_type], [self.num_bytes_int, self.num_bytes_str]), self.AB_ds) 
+
+        
 
     
 if __name__ == '__main__':
