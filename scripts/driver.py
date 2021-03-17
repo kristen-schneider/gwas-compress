d73f666fd4039e85ca0c7dd370a57e8d4106a35a
kristen-schneider <kristen.schneider@colorado.edu>
2021-03-17 17:01:45 -0600

    driver script does it all, sans the compressed header and sans difficult data

--- scripts/driver.py
+++ scripts/driver.py
@@ -7,10 +7,11 @@ import deserialize
 
 
 IN_FILE = '/Users/kristen/Desktop/compression_sandbox/toy_data/10-lines-tab.tsv'
-BLOCK_SIZE = 4
+OUT_FILE = '/Users/kristen/Desktop/compression_sandbox/toy_data/kristen-out.tsv'
+BLOCK_SIZE = 5
 BYTE_SIZES = {int: 5, float: 8, bool: 5, str: 5}
 
-def write_new_file(in_file, block_size):
+def header_and_compress(in_file, block_size):
     '''
     takes a file, converts to funnel format, serializes and compresses each block, write to out_file 
     
@@ -22,18 +23,23 @@ def write_new_file(in_file, block_size):
     write to file
 
     '''
-
-    # [delimeter, [col_names], [col_types], num_cols]
+    endpoints_compresseddata = []
+    
+    # file_info = [delimeter, [col_names], [col_types], num_cols]
     file_info = file_header.get_file_data(IN_FILE)
     delimeter = file_info[0]
     col_names = file_info[1]
     col_types = file_info[2]
     num_columns = file_info[3]
-    print('header: ', file_info, '\n')
-    
+        
+
     funnel_format_data = funnel_format.make_all_blocks(IN_FILE, BLOCK_SIZE, num_columns, delimeter)
+    print(funnel_format_data, '\n\n')
     num_blocks = len(funnel_format_data)
-    print('funnel_format:\n')
+
+    compressed_block_sizes = []
+    block_size_last = []
+    all_compressed_data = b''
     
     # for each block
         # serialize and compress
@@ -41,16 +47,116 @@ def write_new_file(in_file, block_size):
     for b in range(num_blocks):
         curr_block = funnel_format_data[b]
         block_size = len(funnel_format_data[b][0])
-        print(b, curr_block, block_size)     
-        
+        if block_size not in block_size_last: block_size_last.append(block_size)        
+
         s_block = serialize.serialize_list_columns(curr_block, BYTE_SIZES)
         c_block = compress.compress_data(s_block, 0)
-        dc_block = decompress.decompress_data(c_block)
-        ds_block = deserialize.deserialize_block_bitstring(dc_block, block_size, col_types, BYTE_SIZES)
-        print(ds_block) 
+        all_compressed_data+=c_block
+        
+        c_block_size = len(c_block)
+        compressed_block_sizes.append(c_block_size)
+        #dc_block = decompress.decompress_data(c_block)
+        #ds_block = deserialize.deserialize_block_bitstring(dc_block, block_size, col_types, BYTE_SIZES)
+        #print(ds_block) 
     #blocks = funnel_format.make_all_blocks(IN_FILE, BLOCK_SIZE, NUM_COLUMNS)
     
-    return 0
+#    end_points = get_end_points(compressed_block_sizes)
+
+    endpoints_compresseddata.append(compressed_block_sizes)
+    endpoints_compresseddata.append(all_compressed_data)
+    endpoints_compresseddata.append(block_size_last)
+    return endpoints_compresseddata
+
+#def get_end_points(compressed_block_sizes):
+#    c_block_end_pos = [(end) for end in compressed_block_sizes]
+#    print(c_block_end_pos)
+#    return c_block_end_pos
 
 
-write_new_file(IN_FILE, BLOCK_SIZE)
+def write_header_compressed(in_file, block_size):
+    o_file = open(OUT_FILE, 'wb')
+
+    #file_header = [delimeter, [col_names], [col_types], num_cols, [end_pos], [block_sizes]]
+    header = []
+
+    # file_info = [delimeter, [col_names], [col_types], num_cols]
+    file_info = file_header.get_file_data(IN_FILE)
+    delimeter = file_info[0]
+    col_names = file_info[1]
+    col_types = file_info[2]
+    num_columns = file_info[3]
+        
+    endpos_compresseddata = header_and_compress(in_file, block_size)
+    end_positions = endpos_compresseddata[0]
+    compressed_blocks = endpos_compresseddata[1]
+    block_size_last = endpos_compresseddata[2]
+
+    header = [delimeter, col_names, col_types, num_columns, end_positions, block_size_last]
+    #o_file.write(compress.compress_data(bytes(header), 0))
+    o_file.write(compressed_blocks)
+    o_file.close()
+    return header
+
+def read_compressed_file(out_file, header):
+    #print(header)
+    delimeter = header[0]
+    col_names = header[1]
+    col_types = header[2]
+    num_cols = header[3]
+    end_positions = header[4]
+    block_sizes = header[5]
+
+    with open(out_file, 'rb') as f_open:
+        compressed_data = f_open.readline()
+      #  print(compressed_data)
+    f_open.close
+    
+    curr_start = 0
+    for block_i in range(len(end_positions)):
+        if block_i < len(end_positions)-1: curr_block_size = block_sizes[0]
+        else: curr_block_size = block_sizes[1]
+        curr_end = curr_start + end_positions[block_i]
+
+        curr_bitstring = compressed_data[curr_start:curr_end]
+      #  print(curr_bitstring)
+        dc_bitstring = decompress.decompress_data(curr_bitstring)
+        ds_bitstring = deserialize.deserialize_block_bitstring(dc_bitstring, curr_block_size, col_types, BYTE_SIZES)
+        print(ds_bitstring)
+
+        curr_start = curr_end
+    
+    
+    
+
+#def read_decompress_deseralize(in_file, block_size):
+
+    # file_info = [delimeter, [col_names], [col_types], num_cols]
+#    file_info = file_header.get_file_data(IN_FILE)
+#    delimeter = file_info[0]
+#    col_names = file_info[1]
+#    col_types = file_info[2]
+#    num_columns = file_info[3]
+#
+#    endpos_compresseddata = header_and_compress(in_file, block_size)
+#    end_positions = endpos_compresseddata[0]
+#    compressed_blocks = endpos_compresseddata[1]
+#    block_size_last = endpos_compresseddata[2]
+#    curr_start = 0
+#
+#    for block_i in range(len(end_positions)):
+#        if block_i < len(end_positions)-1: curr_block_size = block_size_last[0]
+#        else: curr_block_size = block_size_last[1]
+#        curr_end = curr_start + end_positions[block_i]
+#        
+#        curr_bitstring = compressed_blocks[curr_start:curr_end]
+#        #print(curr_bitstring)
+#        dc_bitstring = decompress.decompress_data(curr_bitstring)
+#        ds_bitstring = deserialize.deserialize_block_bitstring(dc_bitstring, curr_block_size, col_types, BYTE_SIZES) 
+#        print(ds_bitstring)
+#
+#        curr_start = curr_end
+ 
+
+header=write_header_compressed(IN_FILE, BLOCK_SIZE)
+read_compressed_file(OUT_FILE, header)
+#read_decompress_deseralize(IN_FILE, BLOCK_SIZE)
