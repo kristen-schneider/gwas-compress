88572f25017b019191aefad14adf5b3129978be9
kristen-schneider <kristen.schneider@colorado.edu>
2021-03-29 19:40:02 -0600

    added some print statments for time stamps

--- scripts/squish.py
+++ scripts/squish.py
@@ -1,5 +1,6 @@
 # IMPORTS
 import sys
+from datetime import datetime
 import header_generate
 import funnel_format
 import type_handling
@@ -11,15 +12,16 @@ import header_compress_decompress
 # 1. input file
 #IN_FILE = '/Users/kristen/Desktop/compression_sandbox/toy_data/10-lines-tab.tsv'
 #IN_FILE = '/Users/kristen/Desktop/compression_sandbox/toy_data/15.tsv'
-    #IN_FILE = '/Users/kristen/Desktop/compression_sandbox/toy_data/copy-10-lines-tab.tsv'
+IN_FILE = '/Users/kristen/Desktop/compression_sandbox/toy_data/copy-10-lines-tab.tsv'
 # FIJI
 #IN_FILE = '/scratch/Users/krsc0813/gwas-compress/data/test-gwas-data/test.tsv'
-IN_FILE = '/scratch/Users/krsc0813/gwas-compress/data/test-gwas-data/big_test.tsv'
+#IN_FILE = '/scratch/Users/krsc0813/gwas-compress/data/test-gwas-data/big_test.tsv'
 #IN_FILE = '/scratch/Users/krsc0813/gwas-compress/data/full-gwas-data/prescriptions-thiamine-both_sexes_copy.tsv'
+
 # 2. output file
-#OUT_FILE = '/Users/kristen/Desktop/compression_sandbox/toy_data/'
+OUT_FILE = '/Users/kristen/Desktop/compression_sandbox/toy_data/'
 # FIJI
-OUT_FILE = '/scratch/Users/krsc0813/gwas-compress/data/compressed/'
+#OUT_FILE = '/scratch/Users/krsc0813/gwas-compress/data/compressed/'
 # 3. block size
 BLOCK_SIZE = int(sys.argv[1])
 #BLOCK_SIZE = 100000
@@ -28,6 +30,17 @@ DATA_TYPE_CODE_BOOK = {int: 1, float: 2, str: 3, bytes:4}
 DATA_TYPE_BYTE_SIZES = {1: 5, 2: 8, 3: 5, 4:None}
 
 def main():
+
+    full_header = get_full_header()
+
+
+    with open(OUT_FILE + 'kristen-' + str(BLOCK_SIZE) + '-out.tsv', 'rb') as compressed_file:
+        all_content = compressed_file.read()
+    compressed_file.close()
+
+
+
+def get_full_header():
     # header_start
     # Magic number, version number, delimiter, column labels, column types, number columns, gzip header
     # [1,
@@ -37,7 +50,13 @@ def main():
     # [1, 1, 3, 3, 2, 2, 2, 2, 2, 3],
     # 10,
     # b'\x1f\x8b\x08\x00\x00\x00\x00\x00\x02\xff']
+    print('generating start of header...')
+    header_start_START = datetime.now()
     header_start = header_generate.get_header_data(IN_FILE, DATA_TYPE_CODE_BOOK)
+    header_start_END = datetime.now()
+    header_start_TIME = header_start_END-header_start_START
+    print(str(header_start_TIME) + ' for header start to compute...\n')
+
     magic_number = header_start[0]
     version = header_start[1]
     delimiter = header_start[2]
@@ -50,15 +69,23 @@ def main():
     # list of blocks [[block1][block2]...[blockn]]
     # --> a block is a list of columns: block1 = [[col1], [co2]...[colm]]
     # -----> a column is a list of string values: col1 = ['1','1','1','1','1']
+    print('generating funnel format...')
+    funnel_format_START = datetime.now()
     funnel_format_data = funnel_format.make_all_blocks(IN_FILE, BLOCK_SIZE, number_columns, delimiter)
-    print('funnel format complete')    
+    funnel_format_END = datetime.now()
+    funnel_format_TIME = funnel_format_END - funnel_format_START
+    print(str(funnel_format_TIME) + ' for funnel format to compute...\n')
 
+    print('compressing data...')
+    header_end_START = datetime.now()
     header_end = serialize_and_compress_funnel_format(funnel_format_data, column_types)
-    print('header end/serialize and compress funnel complete')
+    header_end_END = datetime.now()
+    header_end_TIME = header_end_END - header_end_START
+    print(str(header_end_TIME) + ' for full compression to complete...\n')
 
     full_header = header_start+header_end
-    print('full header complete')
 
+    print('FULL HEADER:')
     print(full_header)
 
     # w_file = open(OUT_FILE + 'kristen-' + str(BLOCK_SIZE) + '-out.tsv', 'ab')
@@ -66,7 +93,7 @@ def main():
     # c_s_full_header = header_compress_decompress.compress_header(full_header, full_header_types)[1]
     # w_file.write(c_s_full_header)
     # w_file.close()
-    #return full_header
+    return full_header
 
 
 def serialize_and_compress_funnel_format(ff, column_types):
@@ -85,6 +112,9 @@ def serialize_and_compress_funnel_format(ff, column_types):
     block_length = 0
     # go through data, and compress each column
     for block_i in range(len(ff)):
+        print('block' + str(block_i))
+        block_i_START = datetime.now()
+
         num_rows_in_block = len(ff[block_i][0])
 
         curr_block = ff[block_i]
@@ -108,15 +138,13 @@ def serialize_and_compress_funnel_format(ff, column_types):
             # add length of this column to lengths of columns in this block
             curr_compressed_column_end +=len(s_c_column)
             block_column_ends.append(curr_compressed_column_end)
-            # header_end_value += curr_compressed_column_length
-            # block_end_value += curr_compressed_column_length
+
 
         # this should only be triggered for first block and last block.
         if num_rows_in_block not in block_sizes:
             block_sizes.append(num_rows_in_block)
 
         # write the compressed block header and compressed block to the file
-        # print(block_column_ends)
         s_block_header = serialize.serialize_list(block_column_ends, 1, DATA_TYPE_BYTE_SIZES[1])
         s_c_block_header = compress.compress_data(s_block_header, 0)[10:]
 
@@ -132,14 +160,14 @@ def serialize_and_compress_funnel_format(ff, column_types):
         block_end_positions.append(block_end_value)
 
 
-
-        #compressed_length_curr_block_header = len(s_c_block_header)
-            #block_header_lengths.append(compressed_length_curr_block_header)
-
         # print('header', len(s_c_block_header), s_c_block_header)
         w_file.write(s_c_block_header)
         # print('data', len(compressed_block), compressed_block)
         w_file.write(compressed_block)
+
+        block_i_END = datetime.now()
+        block_i_TIME = block_i_END - block_i_START
+        print(str(block_i_TIME) + ' for block ' + str(block_i) + ' to compress and write...\n')
 
         #block_end_positions = header_generate.get_block_end_positions(block_lengths, block_header_lengths)
     if len(block_sizes) < 2: block_sizes.append(num_rows_in_block)
