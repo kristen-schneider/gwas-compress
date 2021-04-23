f515380c27228950df977ca1ce1ffe47ffb64ae2
Kristen Schneider <krsc0813@mendel.colorado.edu>
2021-04-23 14:47:49 -0600

    checking decomp values

--- scripts/pyfastpfor_test_single.py
+++ scripts/pyfastpfor_test_single.py
@@ -3,18 +3,21 @@ import numpy as np
 
 def main():
     #dan()
-    codec = 'simple16'
+    codec = 'BP32'
     arr = [1]*128
-    r = codecs_compression(codec, arr)
+    arr_size = len(arr)
+    comp = np.zeros(arr_size+(16*10), dtype = np.uint32, order = 'C')
+    decomp = np.zeros(arr_size, dtype = np.uint32, order = 'C')
+    r = codecs_compression(codec, arr, comp, decomp)
 
-def codecs_compression(codec,arr):
+def codecs_compression(codec, arr, comp, decomp):
     # array to numpy array
     arr_size = len(arr)
     np_arr = np.array(arr, dtype = np.uint32, order = 'C')
     
     # reserve space for compression and decompression
-    comp = np.zeros(arr_size+(16*10), dtype = np.uint32, order = 'C')
-    decomp = np.zeros(arr_size, dtype = np.uint32, order = 'C')
+    #comp = np.zeros(arr_size+(16*10), dtype = np.uint32, order = 'C')
+    #decomp = np.zeros(arr_size, dtype = np.uint32, order = 'C')
     
     # codec: compression type
     codec_method = getCodec(codec)
