65d9617a5415657aba47a528765529622ba51b9f
Kristen Schneider <krsc0813@mendel.colorado.edu>
2021-04-23 09:38:36 -0600

    failing tests again after new array sizes and malloc

--- scripts/pyfastpfor_test_single.py
+++ scripts/pyfastpfor_test_single.py
@@ -3,10 +3,9 @@ import numpy as np
 
 def main():
     #dan()
-    codec = 'BP32'
-    arr = [1,1,1,1,1]
+    codec = 'simple16'
+    arr = [1]*128
     r = codecs_compression(codec, arr)
-    for i in r: print(i)
 
 def codecs_compression(codec,arr):
     # array to numpy array
@@ -14,73 +13,31 @@ def codecs_compression(codec,arr):
     np_arr = np.array(arr, dtype = np.uint32, order = 'C')
     
     # reserve space for compression and decompression
-    comp = np.zeros(arr_size*2, dtype = np.uint32, order = 'C')
+    comp = np.zeros(arr_size+(16*10), dtype = np.uint32, order = 'C')
     decomp = np.zeros(arr_size, dtype = np.uint32, order = 'C')
     
     # codec: compression type
     codec_method = getCodec(codec)
-
+    # BUG
+    #print('arr: ', arr)
+    #print('comp: ', comp)
+    #print('decomp: ', decomp)
+    
     # compression
     comp_size = codec_method.encodeArray(np_arr, arr_size, comp, len(comp))
+    # BUG
+    #print('arr: ', arr)
+    #print('comp: ', comp)
+    #print('decomp: ', decomp)
     
     # decompression
     codec_method.decodeArray(comp, comp_size, decomp, arr_size)
-
-    return decomp
-
-def compress():
-    #print('arr: ', np_arr)
+    # BUG
+    #print('arr: ', arr)
     #print('comp: ', comp)
     #print('decomp: ', decomp)
-    #assert(arr_size == codec.decodeArray(comp, comp_size, decomp, arr_size))
-    comp_size = codec.encodeArray(np_arr, arr_size, comp, len(comp))
 
-def decompress():
-    codec.decodeArray(comp, comp_size, decomp, arr_size)
-    #print('arr: ', np_arr)
-    #print('comp: ', comp)
-    #print('decomp: ', decomp)
-    assert(np.all(decomp == np_arr))
+    #return decomp
 
-
-def trash():
-    #print(arr)
-    # codecs list minus thee 4 codecs that generate an error
-    #codecs_list = ['BP32', 'copy', 'fastbinarypacking16', 'fastbinarypacking32', 'fastbinarypacking8', 'fastpfor128', 'fastpfor256', 'maskedvbyte', 'newpfor', 'optpfor', 'pfor', 'pfor2008', 'simdbinarypacking', 'simdfastpfor128', 'simdfastpfor256', 'simdgroupsimple', 'simdgroupsimple_ringbuf', 'simdnewpfor', 'simdoptpfor', 'simdpfor', 'simdsimplepfor', 'simple8b', 'simple8b_rle', 'simplepfor', 'streamvbyte', 'varint', 'varintg8iu', 'varintgb', 'vbyte']
-    # full codecs list
-    #codecs_list = ['BP32', 'copy', 'fastbinarypacking16', 'fastbinarypacking32', 'fastbinarypacking8', 'fastpfor128', 'fastpfor256', 'maskedvbyte', 'newpfor', 'optpfor', 'pfor', 'pfor2008', 'simdbinarypacking', 'simdfastpfor128', 'simdfastpfor256', 'simdgroupsimple', 'simdgroupsimple_ringbuf', 'simdnewpfor', 'simdoptpfor', 'simdpfor', 'simdsimplepfor', 'simple16', 'simple8b', 'simple8b_rle', 'simple9', 'simple9_rle', 'simplepfor', 'streamvbyte', 'varint', 'varintg8iu', 'varintgb', 'vbyte', 'vsencoding']
-    
-    #performance_file = open(out_f+'codecs_performance-' + str(col_num) + '.tsv', 'w')
-    #performance_file.truncate()
-    #print(col_num, arr, out_f)
-    
-    # in array
-    #arr = [1]*200#,1,1,1,1]
-    arr_size = len(arr)
-    np_arr = np.array(arr, dtype = np.uint32, order = 'C')
-        
-    # reserve space for compression and decompression
-    comp = np.zeros(arr_size*2, dtype = np.uint32, order = 'C')
-    decomp = np.zeros(arr_size, dtype = np.uint32, order = 'C')
-        
-    # codec: compression type
-    c = 'simple9'
-    codec = getCodec(c)
-        
-    # compress data
-    comp_size = codec.encodeArray(np_arr, arr_size, comp, len(comp))
-    print(c + ': ', 'compression ratio: %g' % (float(comp_size)/arr_size))
-    
-    # decompress data
-    #print('arr: ', np_arr)
-    #print('comp: ', comp)
-    #print('decomp: ', decomp)
-    #assert(arr_size == codec.decodeArray(comp, comp_size, decomp, arr_size))
-    codec.decodeArray(comp, comp_size, decomp, arr_size)
-    #print('arr: ', np_arr)
-    #print('comp: ', comp)
-    #print('decomp: ', decomp)
-    assert(np.all(decomp == np_arr))
- 
 if __name__ == '__main__':
     main()
--- scripts/test_codecs.py
+++ scripts/test_codecs.py
@@ -6,9 +6,9 @@ class TestCodecs(unittest.TestCase):
 
     codecs_list = ['BP32', 'copy', 'fastbinarypacking16', 'fastbinarypacking32', 'fastbinarypacking8', 'fastpfor128', 'fastpfor256', 'maskedvbyte', 'newpfor', 'optpfor', 'pfor', 'pfor2008', 'simdbinarypacking', 'simdfastpfor128', 'simdfastpfor256', 'simdgroupsimple', 'simdgroupsimple_ringbuf', 'simdnewpfor', 'simdoptpfor', 'simdpfor', 'simdsimplepfor', 'simple16', 'simple8b', 'simple8b_rle', 'simple9', 'simple9_rle', 'simplepfor', 'streamvbyte', 'varint', 'varintg8iu', 'varintgb', 'vbyte', 'vsencoding']
     
-    short_arr = [1,1,1,1,1]
-    long_arr = [1]*200
-    large_arr = [321489, 98172, 193012, 736280]
+    short_arr = [1]*128
+    long_arr = [1]*20000
+    large_arr = np.array(np.random.randint(10000, 600000, 128), dtype = np.uint32, order = 'C')
 
     def test_BP32(self):
         short_c = pyfastpfor_test_single.codecs_compression('BP32', self.short_arr)
@@ -241,15 +241,16 @@ class TestCodecs(unittest.TestCase):
         assert(np.all(long_c == long_a)) 
         assert(np.all(large_c == large_a))
    
-    def test_simple16(self):
-        short_c = pyfastpfor_test_single.codecs_compression('simple16', self.short_arr)
-        short_a = np.array(self.short_arr, dtype = np.uint32, order = 'C')
-        long_c = pyfastpfor_test_single.codecs_compression('simple16', self.long_arr)
-        long_a = np.array(self.long_arr, dtype = np.uint32, order = 'C')
-        large_c = pyfastpfor_test_single.codecs_compression('simple16', self.large_arr)
-        large_a = np.array(self.large_arr, dtype = np.uint32, order = 'C')
-        assert(np.all(short_c == short_a))
-        assert(np.all(long_c == long_a)) 
-        assert(np.all(large_c == large_a))
+    #def test_simple16(self):
+        #short_c = pyfastpfor_test_single.codecs_compression('simple16', self.short_arr)
+        #short_a = np.array(self.short_arr, dtype = np.uint32, order = 'C')
+        #long_c = pyfastpfor_test_single.codecs_compression('simple16', self.long_arr)
+        #long_a = np.array(self.long_arr, dtype = np.uint32, order = 'C')
+        #large_c = pyfastpfor_test_single.codecs_compression('simple16', self.large_arr)
+        #large_a = np.array(self.large_arr, dtype = np.uint32, order = 'C')
+        #assert(np.all(short_c == short_a))
+        #assert(np.all(long_c == long_a)) 
+        #assert(np.all(large_c == large_a))
+
 if __name__ == '__main__':
     unittest.main()
