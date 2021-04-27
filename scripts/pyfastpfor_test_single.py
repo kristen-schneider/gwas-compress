a573f8fe795c54ccbcc4714ab060c6a6ba27a26f
Kristen Schneider <krsc0813@mendel.colorado.edu>
2021-04-27 13:53:27 -0600

    inconsistent errors running same script. pushing for clone

--- scripts/pyfastpfor_test_single.py
+++ scripts/pyfastpfor_test_single.py
@@ -6,23 +6,25 @@ def main():
     codec = 'BP32'
     arr_size = 128*32
     arr = [1]*arr_size
-    arr = np.ones(arr_size, dtype = np.uint32, order = 'C')
-    comp = np.zeros(arr_size+(16*10), dtype = np.uint32, order = 'C')
-    decomp = np.zeros(arr_size, dtype = np.uint32, order = 'C')    
+    buffer_size = 150*16
+    #arr = np.ones(arr_size, dtype = np.uint32, order = 'C')
+    #comp = np.zeros(arr_size+(16*10), dtype = np.uint32, order = 'C')
+    #decomp = np.zeros(arr_size, dtype = np.uint32, order = 'C')    
     #r = codecs_compression(codec, arr, comp, decomp)
-    #kristen(codec, arr, arr_size)
-    dan()
-    #dan2(codec, arr_size)
+    kristen(codec, arr, arr_size, buffer_size)
+    #dan()
+    #dan2(codec, arr_size, buffer_size)
 
-def kristen(codec, arr, arr_size):
+def kristen(codec, arr, arr_size, buffer_size):
     np_arr = np.array(arr, dtype = np.uint32, order = 'C')
-    comp = np.zeros(arr_size+(16*10), dtype = np.uint32, order = 'C')
+    comp = np.zeros(arr_size+buffer_size, dtype = np.uint32, order = 'C')
     decomp = np.zeros(arr_size, dtype = np.uint32, order = 'C')
     codec_method = getCodec(codec)
     comp_size = codec_method.encodeArray(np_arr, arr_size, comp, len(comp))
     decomp_size = codec_method.decodeArray(comp, comp_size, decomp, arr_size)    
-
-
+    print('codec: ', codec)
+    print('compression ratio: ', float(comp_size)/arr_size)
+    return decomp_size
 
 def codecs_compression(codec, arr, comp, decomp):
     print(codec) 
@@ -58,13 +60,13 @@ def codecs_compression(codec, arr, comp, decomp):
 
 def dan():
     arrSize = 128 * 32
-    maxVal = 10
-
-    inp = np.array(np.random.randint(0, maxVal, arrSize), dtype = np.uint32, order = 'C')
+    maxVal = 1000
+    #inp = np.zeros(arrSize, dtype = np.uint32, order = 'C')
+    inp = np.array(np.random.randint(100, maxVal, arrSize), dtype = np.uint32, order = 'C')
     print(inp)
     inpCompDecomp = np.zeros(arrSize, dtype = np.uint32, order = 'C')
 
-    inpComp = np.zeros(arrSize + (16*150), dtype = np.uint32, order = 'C')
+    inpComp = np.zeros(arrSize + (16*1000), dtype = np.uint32, order = 'C')
     
     codec = getCodec('simple16')
     
@@ -75,14 +77,15 @@ def dan():
     assert(arrSize == codec.decodeArray(inpComp, compSize, inpCompDecomp, arrSize))
     assert(np.all(inpCompDecomp == inp))
 
-def dan2(inCodec, arrSize):
+def dan2(inCodec, arr, arrSize, buffer_size):
     #arrSize = 128 * 32
     #maxVal = 2048
 
-    inp = np.array(np.random.randint(0,10,arrSize), dtype = np.uint32, order = 'C')
+    inp = np.array(arr, dtype = np.uint32, order = 'C')
+    #inp = np.array(np.random.randint(0,10,arrSize), dtype = np.uint32, order = 'C')
     inpCompDecomp = np.zeros(arrSize, dtype = np.uint32, order = 'C')
 
-    inpComp = np.zeros(arrSize + 1024, dtype = np.uint32, order = 'C')
+    inpComp = np.zeros(arrSize + buffer_size, dtype = np.uint32, order = 'C')
     
     codec = getCodec(inCodec)
     
