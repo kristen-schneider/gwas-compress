68324c40b6a4d5baca9abeb93f539de23039b2d8
Kristen Schneider <krsc0813@mendel.colorado.edu>
2021-04-29 10:16:07 -0600

    writing codec compression-block-1 compression-block-2 compression-block-3 etc.

--- scripts/block_pyfast.py
+++ scripts/block_pyfast.py
@@ -8,14 +8,16 @@ import type_handling
 #[['1', '1', '1'], ['58396', '62745', '63668'], ['T', 'C', 'G'], ['C', 'G', 'A'], ['0.000e+00', 'NA', '0.000e+00'], ['2.409e-04', 'NA', '2.780e-05'], ['-1.007e+00', 'NA', '-1.287e+00'], ['1.437e+00', 'NA', '4.597e+00'], ['4.833e-01', 'NA', '7.795e-01'], ['true', 'NA', 'true']]
 
 
-IN_FILE = '/home/krsc0813/projects/gwas-compress/data/million.tsv'
-OUT_FILE = '/home/krsc0813/projects/gwas-compress/plot_data/'
+IN_FILE = '/home/krsc0813/projects/gwas-compress/data/ten.tsv'
+OUT_CSV = '/home/krsc0813/projects/gwas-compress/plot_data/test.csv'
 BLOCK_SIZE = 10000
 NUM_COLS = 10
 DELIMITER = '\t'
 COL_TYPES = [1, 1, 3, 3, 2, 2, 2, 2, 2, 3]
 
 def main():
+    csv_codec_column(getCodecList())
+    
     ff = generate_funnel_format.make_all_blocks(IN_FILE, BLOCK_SIZE, NUM_COLS, DELIMITER)
     for block_i in range(len(ff)):
         curr_block = ff[block_i]
@@ -24,12 +26,12 @@ def main():
             column_type = COL_TYPES[column_i]
             typed_column = type_handling.convert_to_type(curr_column, column_type)
 
-
+            # only try these codecs on integer compression
             if column_type == 1:
                 #print(column_i, typed_column)
             
                 #print('og: ', typed_column)
-                decomp = pyfastpfor_sandbox.kristen('BP32', typed_column, len(typed_column), 15*16)
+                decomp = pyfastpfor_sandbox.kristen('BP32', typed_column, len(typed_column), 15*16, OUT_CSV)
                 #print('decomp: ', decomp)
                 
     #arr = [1] * 200
--- scripts/pyfastpfor_sandbox.py
+++ scripts/pyfastpfor_sandbox.py
@@ -1,5 +1,6 @@
 from pyfastpfor import *
 import numpy as np
+import csv
 
 def main():
     #dan()
@@ -7,15 +8,12 @@ def main():
     arr_size = 128*32
     arr = [1]*arr_size
     buffer_size = 15*16
-    #arr = np.ones(arr_size, dtype = np.uint32, order = 'C')
-    #comp = np.zeros(arr_size+(16*10), dtype = np.uint32, order = 'C')
-    #decomp = np.zeros(arr_size, dtype = np.uint32, order = 'C')    
-    #r = codecs_compression(codec, arr, comp, decomp)
-    kristen(codec, arr, arr_size, buffer_size)
-    #dan(codec)
-    dan2(codec)
+    out_csv = '/home/krsc0813/projects/gwas-compress/scripts/TEST.csv'
+    csv_codec_column(getCodecList(), out_csv)
+    kristen(codec, arr, arr_size, buffer_size, out_csv)
 
-def kristen(codec, arr, arr_size, buffer_size):
+def kristen(codec, arr, arr_size, buffer_size, out_csv):
+    # prepare output file
     np_arr = np.array(arr, dtype = np.uint32, order = 'C')
     comp = np.zeros(arr_size+buffer_size, dtype = np.uint32, order = 'C')
     decomp = np.zeros(32+arr_size, dtype = np.uint32, order = 'C')
@@ -24,50 +22,28 @@ def kristen(codec, arr, arr_size, buffer_size):
     decomp_size = codec_method.decodeArray(comp, comp_size, decomp, arr_size)    
     #print('codec: ', codec)
     #print('arr: ', np_arr)
-    print('compression ratio: ', float(comp_size)/arr_size)
+    
+    compression_ratio = float(comp_size)/arr_size
+    #with open(out_csv, 'w') as o:
+    #    writer = csv.writer(o, lineterminator='\n')
+        #writer.write
+        #row = next(writer)
+        #print(row)
+        #row.append()
+        #if first_block
+        #print('compression ratio: ', float(comp_size)/arr_size)
+   
     #print('decomp arr: ', decomp)
     return decomp_size
 
-def dan(curr_codec):
-    arrSize = 128 * 32
-    maxVal = 2048
-    inp = np.array(np.random.randint(0, maxVal, arrSize), dtype = np.uint32, order = 'C')
-    inpCompDecomp = np.zeros(32+arrSize, dtype = np.uint32, order = 'C')
-    inpComp = np.zeros(arrSize + (1024), dtype = np.uint32, order = 'C')
-    
-    codec = getCodec(curr_codec)
-    
-    compSize = codec.encodeArray(inp, arrSize, inpComp, len(inpComp))
-     
-    #print('Compression ratio: %g' % (float(compSize)/arrSize))
-   
-    codec.decodeArray(inpComp, compSize, inpCompDecomp, arrSize) 
-    #assert(arrSize == codec.decodeArray(inpComp, compSize, inpCompDecomp, arrSize))
-    #assert(np.all(inpCompDecomp == inp))
-    #print('inpCompDecomp: ', len(inpCompDecomp), inpCompDecomp)
 
-def dan2(curr_codec):
-    arrSize = 128 * 32
-    maxVal = 2048
-    inp = np.ones(arrSize, dtype = np.uint32, order = 'C')
-    #inp = np.zeros(arrSize, dtype = np.uint32, order = 'C')
-    #inp = np.array(np.random.randint(0, maxVal, arrSize), dtype = np.uint32, order = 'C')
-    #print('inp: ', len(inp), inp)
-    inpCompDecomp = np.zeros(32+arrSize, dtype = np.uint32, order = 'C')
-    inpComp = np.zeros(arrSize + (1024), dtype = np.uint32, order = 'C')
-    
-    codec = getCodec(curr_codec)
-    
-    compSize = codec.encodeArray(inp, arrSize, inpComp, len(inpComp))
-    #print('inpComp: ', len(inpComp), inpComp)
-     
-    #print('Compression ratio: %g' % (float(compSize)/arrSize))
-   
-    codec.decodeArray(inpComp, compSize, inpCompDecomp, arrSize) 
-    #assert(arrSize == codec.decodeArray(inpComp, compSize, inpCompDecomp, arrSize))
-    #assert(np.all(inpCompDecomp == inp))
-    #print('inpCompDecomp: ', len(inpCompDecomp), inpCompDecomp)
-
+def csv_codec_column(codec_list, out_csv):
+    with open(out_csv, 'w') as o:
+        csv_writer = csv.writer(o, lineterminator='\n')
+        for codec in codec_list:
+            
+            #print(row)
+            csv_writer.writerow([codec])
 
 
 if __name__ == '__main__':
