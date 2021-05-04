a64eb2bc658f87f7bc18ed3b7b1e50f7ba50a8f3
kristen-schneider <kristen.schneider@colorado.edu>
2021-05-04 12:16:35 -0600

    adding functionality for bz2

--- scripts/compress.py
+++ scripts/compress.py
@@ -1,5 +1,6 @@
 import gzip
 import zlib
+import bz2
 
 def compress_data(compression_method, s_bitstring, time):
     '''
@@ -23,6 +24,10 @@ def compress_data(compression_method, s_bitstring, time):
     elif compression_method == 2:
         c_bitstring = zlib_compress(s_bitstring)
         header = 0
+    # BZ2
+    elif compression_method == 3:
+        c_bitstring = bz2_compress(s_bitstring)
+        header = 4
 
     return c_bitstring, header
 
@@ -43,6 +48,21 @@ def gzip_compress(s_bitstring, time):
     return c_bitstring
 
 def zlib_compress(s_bitstring):
+    '''
+    uses python's zlib.compress to compress a serialized bitstring
+
+    INPUT
+    s_bitstring = serialized bitstring from the serialize_data method in serialize.py
+
+    OUTPUT
+    c_bitstring = compressed bitstring (using python's zlib.compress() function)
+
+    '''
+
+    c_bitstring = zlib.compress(s_bitstring)
+    return c_bitstring
+
+def bz2_compress(s_bitstring):
     '''
     uses python's zlib.compress to compress a serialized bitstring
 
