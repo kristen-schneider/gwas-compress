382be5127d7f24a95edb42b1639a0e35cc36283f
kristen-schneider <kristen.schneider@colorado.edu>
2021-05-05 13:22:10 -0600

    header_compress making room for all different compression headers

--- scripts/header_compress.py
+++ scripts/header_compress.py
@@ -25,22 +25,21 @@ def full_header_tools(data_type_code_book, data_type_byte_sizes, full_header):
     header_types = get_header_types(full_header, data_type_code_book)
     serialized_header_types = serialize.serialize_list(header_types, data_type_code_book[int], data_type_byte_sizes[data_type_code_book[int]])
 
-    # get header ends and serialize
-    header_ends_data = get_header_ends_and_serialize(data_type_byte_sizes, full_header, header_types)
-
     # get number of elements in each header item
     header_num_elements = get_header_lengths(full_header)
     serialized_header_num_elements = serialize.serialize_list(header_num_elements, data_type_code_book[int], data_type_byte_sizes[data_type_code_book[int]])
 
-    header_ends = header_ends_data[1]
+    # get full serialized header as a list
+    serialized_header_list = serialize_header(header_types, data_type_byte_sizes, full_header)
+    # get full serialized header as a bitstring
+    serialized_header = join_serialized_list(serialized_header_list)
+
+    # get ends of each serialized element in column
+    header_ends = get_header_ends(serialized_header_list)
     serialized_header_ends = serialize.serialize_list(header_ends, data_type_code_book[int], data_type_byte_sizes[data_type_code_book[int]])
 
-    # get header data (already serialized)
-    serialized_header_data = header_ends_data[2]
-
-
     return [serialized_header_types, serialized_header_num_elements, serialized_header_ends,
-            serialized_header_data]
+            serialized_header]
 
 def get_header_types(full_header, DATA_TYPE_CODE_BOOK):
     '''
@@ -66,33 +65,40 @@ def get_header_lengths(full_header):
         except TypeError: header_lengths.append(1)
     return header_lengths
 
-def get_header_ends():
-
-
-def get_header_ends_and_serialize(data_type_byte_sizes, full_header, header_types):
+def serialize_header(header_types, data_type_byte_sizes, full_header):
     '''
-    returns
+    returns serialized header as a list
     '''
-    header_num_elements = []
-    header_ends = []
-    serialized_full_header = b''
+    serialized_header_list = []
 
-    h_end = 0
     for h in range(len(full_header)):
         current_h = full_header[h]  # one element of header
-
-        try: length_curr_h = len(current_h)
-        except TypeError: length_curr_h = 1
-        header_num_elements.append(length_curr_h)
 
         # serialize a list and a single value differently
         if type(current_h) == list:
             s_current_h = serialize.serialize_list(current_h, header_types[h], data_type_byte_sizes[header_types[h]])
         else:
             s_current_h = serialize.serialize_data(current_h, header_types[h], data_type_byte_sizes[header_types[h]])
-        h_end += len(s_current_h)
-        header_ends.append(h_end)
 
-        serialized_full_header += s_current_h
+        serialized_header_list.append(s_current_h)
+    return serialized_header_list
 
-    return header_num_elements, header_ends, serialized_full_header
+def join_serialized_list(serialized_header_list):
+    '''
+    returns a full bitstring of a list of bitstrings
+    '''
+    serialized_header = b''
+    for sh in serialized_header_list:
+        serialized_header+=sh
+    return serialized_header
+
+def get_header_ends(serialized_header_list):
+    '''
+    return ends of each serialized element of the header
+    '''
+    end = 0
+    header_ends = []
+    for h in serialized_header_list:
+        header_ends.append(end+len(h))
+        end+=len(h)
+    return header_ends
