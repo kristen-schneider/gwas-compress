1da3f62fb0fdddaaf9a32ef76154ea268cccac67
kristen-schneider <kristen.schneider@colorado.edu>
2021-06-08 16:13:15 -0600

    15 unit tests passing for 15 snvs in a row little endian style <3

--- scripts/ref_alt.py
+++ scripts/ref_alt.py
@@ -1,6 +1,10 @@
 # https://wiki.python.org/moin/BitwiseOperators
 # https://wiki.python.org/moin/BitArrays
 import array
+def main():
+    x = assumption1(True, True, 'C'*15)
+    print(x)
+
 
 def assumption1(fifteen_SNVs_bool, SNVs_bool, SNVs):
     # 1. 1 bit: fifteen consecutive snps
@@ -11,10 +15,6 @@ def assumption1(fifteen_SNVs_bool, SNVs_bool, SNVs):
 
     # assumptions for round 1 is that we pack 16 snvs into one int.
     # assume always 00
-    # fifteen_SNVs_bool = True
-    # SNVs_bool = True
-    # As = 'AAAAAAAAAAAAAAA'
-    # Ts = 'TTTTTTTTTTTTTTT'
     bitstring = bits_one_two(fifteen_SNVs_bool, SNVs_bool)
     if fifteen_SNVs_bool:
         # move over 30 bits
@@ -41,7 +41,7 @@ def bits_one_two(fifteen_SNVs_bool, SNVs_bool):
         # shift over 1.
         bitstring = shift_bit(bitstring, 1)
         # set second bit
-        bitstring = second_bit(SNVs_bool)
+        bitstring = bitstring | second_bit(SNVs_bool)
 
     return bitstring
 
@@ -70,18 +70,19 @@ def second_bit(snv_bool):
         return 1
 
 def shift_bit(bitstring, shift):
+    """
+    shifting for little endian format
+    """
     return bitstring << shift
-    # y = bitstring >> shift
-    # return x,y
 
 
 def encode_SNVs(SNVs):
     snv_bitstring = 0
     for v in range(len(SNVs)):
         # get proper bit representation for the variant
-        snv = get_variant_number(SNVs[v])
+        snv = shift_bit(get_variant_number(SNVs[v]), 2*v)
 
-        # if we are on first snv, set bitsring
+        # if we are on first snv, set bitstring
         if v == 0:
             snv_bitstring = snv
         # # if we are on last snv, no not shift, just return
@@ -91,7 +92,7 @@ def encode_SNVs(SNVs):
         # if we are in middle, shift bitstring
         else:
             # shift bitstring
-            snv_bitstring = shift_bit(snv_bitstring, 2)
+            # snv_bitstring = shift_bit(snv_bitstring, 2*v)
             snv_bitstring = snv_bitstring | snv
     return snv_bitstring
 
@@ -108,71 +109,5 @@ def get_variant_number(base):
         print('not a proper base')
         return -1
 
-
-def print_bit_array_element_be(bit_array_element, size):
-    output = []
-    for i in range(1, size + 1):
-        output.append(get_bit(bit_array_element, i, size))
-
-    print (' '.join([str(x) for x in output]))
-
-
-def get_bit(bit_array_element, n, size):
-    return (bit_array_element >> (size - n)) & 1
-
-
-def set_element_bit(bit_array_element, i, size):
-    # stores the bit array element as the result of an OR with the old bit array and the new bit array with the new bit added
-    bit_array_element = bit_array_element | (1 << (size - i))
-    return bit_array_element
-
-
-def set_bit(B, i, element_size):
-    # chooses which element we want (i.e. chooses one of: 00000000 00000000 00000000)
-    curr_element = (i - 1) / element_size
-    # chooses which bit in our element (i.e. chooses one of: 00000000)
-    curr_i = i - curr_element * element_size;
-
-    B[curr_element] = set_element_bit(B[curr_element], curr_i, element_size)
-
-
-def packed_bit():
-    return 0
-
-
-def single_encoded_bit():
-    return 1
-
-
-# def misc():
-#     for individual in genotype_file:
-#         # in order to grab the variants, you must manipulate the row (i.e. separate by some delimited character
-#         this_row = individual.split(" ")
-#         # print individual
-#
-#         # go through each variant for each individual
-#         v = 1
-#         for variant in this_row:
-#             # get the index value that corresponds to what kind of zygosity this is (i.e. 0 = homo ref, 1 = het, ...)
-#             genotype_array_num = get_genotype_array_num(variant.rstrip())
-#             # print variant.rstrip(), ": ", genotype_array_num
-#             # print i, current_indiv
-#             # extra_step = current_indiv[0]
-#             # print extra_step
-#             # bit_array_for_selected_zygosity = extra_step[genotype_array_num]
-#             # print bit_array_for_selected_zygosity
-#             # print bit_array_for_selected_zygosity[0]
-#             # print v
-#
-#             # gt_to_change = current_indiv[genotype_array_num]
-#             # print B[i][genotype_array_num]
-#             set_bit(B[i][genotype_array_num], v, bit_size)
-#             v += 1
-#         #	print current_indiv, "\n\n"
-#         #	B[i] = current_individual
-#         # print "\n"
-#         i += 1
-
-#
-# if __name__ == '__main__':
-#         main()
+if __name__ == '__main__':
+        main()
--- scripts/test_ref_alt.py
+++ scripts/test_ref_alt.py
@@ -9,6 +9,19 @@ class TestSerializationToDeserialization(unittest.TestCase):
     snpT = 'T'*15
     mix1 = 'ACG'*5
     mix2 = 'ACT'*5
+    mix3 = 'CAT'*5
+    mix4 = 'CGG'*5
+    mix5 = 'GTA'*5
+    mix6 = 'GCC'*5
+    mix7 = 'TTA'*5
+    mix8 = 'TAG'*5
+    mix9 = 'TTAGC'*3
+    mix10 = 'GATAC'*3
+    mix11 = 'CGCAT'*3
+    mix12 = 'ACAAA'*3
+    mix13 = 'ACGTACGTACGTCCC'
+    mix14 = 'CCCCCAAAAACCCCC'
+    mix15 = 'TTTCCCGGGAAATAG'
 
 
     def test_assumption1(self):
@@ -23,13 +36,37 @@ class TestSerializationToDeserialization(unittest.TestCase):
         self.assertEqual(ref_alt.assumption1(fifteen_SNVs_bool, SNVs_bool, self.snpG), 715827882)
         # 00 111111111111111111111111111111
         self.assertEqual(ref_alt.assumption1(fifteen_SNVs_bool, SNVs_bool, self.snpT), 1073741823)
-        # 00 000110000110000110000110000110
-        self.assertEqual(ref_alt.assumption1(fifteen_SNVs_bool, SNVs_bool, self.mix1), 102261126)
-        # 00 000111000111000111000111000111
-        self.assertEqual(ref_alt.assumption1(fifteen_SNVs_bool, SNVs_bool, self.mix1), 119304647)
+        # 00 100100100100100100100100100100
+        self.assertEqual(ref_alt.assumption1(fifteen_SNVs_bool, SNVs_bool, self.mix1), 613566756)
+        # 00 110100110100110100110100110100
+        self.assertEqual(ref_alt.assumption1(fifteen_SNVs_bool, SNVs_bool, self.mix2), 886263092)
+        # 00 110001110001110001110001110001
+        self.assertEqual(ref_alt.assumption1(fifteen_SNVs_bool, SNVs_bool, self.mix3), 835132529)
+        # 00 101001101001101001101001101001
+        self.assertEqual(ref_alt.assumption1(fifteen_SNVs_bool, SNVs_bool, self.mix4), 698784361)
+        # 00 001110001110001110001110001110
+        self.assertEqual(ref_alt.assumption1(fifteen_SNVs_bool, SNVs_bool, self.mix5), 238609294)
+        # 00 010110010110010110010110010110
+        self.assertEqual(ref_alt.assumption1(fifteen_SNVs_bool, SNVs_bool, self.mix6), 374957462)
+        # 00 001111001111001111001111001111
+        self.assertEqual(ref_alt.assumption1(fifteen_SNVs_bool, SNVs_bool, self.mix7), 255652815)
+        # 00 100011100011100011100011100011
+        self.assertEqual(ref_alt.assumption1(fifteen_SNVs_bool, SNVs_bool, self.mix8), 596523235)
+        # 00 011000111101100011110110001111
+        self.assertEqual(ref_alt.assumption1(fifteen_SNVs_bool, SNVs_bool, self.mix9), 418790799)
+        # 00 010011001001001100100100110010
+        self.assertEqual(ref_alt.assumption1(fifteen_SNVs_bool, SNVs_bool, self.mix10), 321177906)
+        # 00 110001100111000110011100011001
+        self.assertEqual(ref_alt.assumption1(fifteen_SNVs_bool, SNVs_bool, self.mix11), 832333593)
+        # 00 000000010000000001000000000100
+        self.assertEqual(ref_alt.assumption1(fifteen_SNVs_bool, SNVs_bool, self.mix12), 4198404)
+        # 00 010101111001001110010011100100
+        self.assertEqual(ref_alt.assumption1(fifteen_SNVs_bool, SNVs_bool, self.mix13), 367322340)
+        # 00 010101010100000000000101010101
+        self.assertEqual(ref_alt.assumption1(fifteen_SNVs_bool, SNVs_bool, self.mix14), 357564757)
+        # 00 100011000000101010010101111111
+        self.assertEqual(ref_alt.assumption1(fifteen_SNVs_bool, SNVs_bool, self.mix15), 587375999)
 
-    # def test_indel(self):
-    #     self.assertEqual(ref_alt.run_length_encoding(self.indel1), 11213141)
 
 if __name__ == '__main__':
     unittest.main()
