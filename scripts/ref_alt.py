f5349840d098f966e97c1e2c1dbb54f74f811550
kristen-schneider <kristen.schneider@colorado.edu>
2021-06-09 14:21:52 -0600

    found an edge case that does not work

--- scripts/ref_alt.py
+++ scripts/ref_alt.py
@@ -2,11 +2,20 @@
 # https://wiki.python.org/moin/BitArrays
 import math
 def main():
-    assumption1('/Users/kristen/Desktop/compression_sandbox/toy_data_in/ref-alt.tsv')
-    # data = ['AAAAA']*1
-    # data = 'ACGT'*7
-    # x = col_input(data)
-    # print(x)
+    # assumption1('/Users/kristen/Desktop/compression_sandbox/toy_data_in/ref-alt.tsv')
+    data = ['T', 'G', 'G', 'C', 'T', 'T', 'T', 'C', 'G', 'A',
+             'CT',
+             'A', 'G', 'T', 'T', 'T', 'T', 'G', 'G', 'G', 'C', 'G', 'T', 'C', 'A', 'A',
+             'T', 'T', 'C', 'A', 'G', 'A', 'C', 'A', 'G', 'G',
+             'A',
+             'AAAAAAAAAAAATATATATATATATATATATATATAT',
+             'G', 'G', 'G', 'T', 'C', 'C', 'A', 'G', 'A', 'C', 'T', 'C', 'T', 'A', 'C',
+             'C', 'G', 'T', 'T', 'C', 'A', 'T', 'C', 'C', 'C',
+             'TA',
+             'C', 'C', 'C', 'A', 'C', 'C', 'C', 'C', 'C', 'G',
+             'ACAGGAGGGCGGG']
+    x = col_input(data)
+    print(x)
 
 def col_input(column):
     """
