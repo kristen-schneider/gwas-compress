2809f89a1e9d9334bcc9726fa9c9fb11939946ec
kristen-schneider <kristen.schneider@colorado.edu>
2021-06-10 17:42:22 -0600

    i restructured how we handle snvs bc i wanted to reuse code. when we had 15 ints, it got weird for 10-15 and i felt like we were reusing space. 13 worked better for reusing code.

--- scripts/ref_alt.py
+++ scripts/ref_alt.py
@@ -3,28 +3,29 @@
 import math
 def main():
     # assumption1('/Users/kristen/Desktop/compression_sandbox/toy_data_in/ref-alt.tsv')
-    data = ['T', 'G', 'G', 'C', 'T', 'T', 'T', 'C', 'G', 'A',
-             'CT',
-             'A', 'G', 'T', 'T', 'T', 'T', 'G', 'G', 'G', 'C', 'G', 'T', 'C', 'A', 'A',
-             'T', 'T', 'C', 'A', 'G', 'A', 'C', 'A', 'G', 'G',
-             'A',
-             'AAAAAAAAAAAATATATATATATATATATATATATAT',
-             'G', 'G', 'G', 'T', 'C', 'C', 'A', 'G', 'A', 'C', 'T', 'C', 'T', 'A', 'C',
-             'C', 'G', 'T', 'T', 'C', 'A', 'T', 'C', 'C', 'C',
-             'TA',
-             'C', 'C', 'C', 'A', 'C', 'C', 'C', 'C', 'C', 'G',
-             'ACAGGAGGGCGGG']
+    # data = ['T', 'G', 'G', 'C', 'T', 'T', 'T', 'C', 'G', 'A',
+    #          'CT',
+    #          'A', 'G', 'T', 'T', 'T', 'T', 'G', 'G', 'G', 'C', 'G', 'T', 'C', 'A', 'A',
+    #          'T', 'T', 'C', 'A', 'G', 'A', 'C', 'A', 'G', 'G',
+    #          'A',
+    #          'AAAAAAAAAAAATATATATATATATATATATATATAT',
+    #          'G', 'G', 'G', 'T', 'C', 'C', 'A', 'G', 'A', 'C', 'T', 'C', 'T', 'A', 'C',
+    #          'C', 'G', 'T', 'T', 'C', 'A', 'T', 'C', 'C', 'C',
+    #          'TA',
+    #          'C', 'C', 'C', 'A', 'C', 'C', 'C', 'C', 'C', 'G',
+    #          'ACAGGAGGGCGGG']
+    data = 'AAA'
     x = col_input(data)
     print(x)
 
 def col_input(column):
     """
-    SNP
+    SNV
     1 5     26
     0 00000 00000000000000000000000000
-    --snps
-    --length of snp
-    --snp bases
+    --snvs
+    --length of snv
+    --snv bases
 
     INDEL
     ** first int **
@@ -40,58 +41,53 @@ def col_input(column):
     --length of bases
     --bases
 
-    takes a full column of ref/alt bases and finds an element to encode; and then encodes it
-        1. 15 SNVs in a row
-        2. < 15 SNVs in a row
-        3. INDEL
-    returns list of integers
+    returns list of integers which represents a column of ref or alt bases (snvs and indels)
     """
-    list_ints = []
-    SNVs = []
+
+    all_ints = []   # to return at end
+
+    SNVs = []   # start collecting SNVs
     for row in column:
         # only SNV
         if len(row) == 1:
             SNVs.append(row)
 
-            # if we achieve 15 SNVs in a row, encode them and move on
-            if len(SNVs) == 15:
+            # if we achieve 13 SNVs in a row, encode them and move on
+            if len(SNVs) == 13:
+                # flag to specify SNVs
+                SNV_flag = shift_bit(0, 31)
+                # SNV length here is 13
+                SNV_length = shift_bit(len(SNVs), 26)
+                # encode all 13 SNVS
                 SNV_bitstring = encode_SNVs(SNVs)
-                start_bitstring = shift_bit(0, 30)
-                full_bitstring = start_bitstring | SNV_bitstring
-                list_ints.append(full_bitstring)
+                # final int
+                full_SNV_bitstring = SNV_flag | SNV_length | SNV_bitstring
+                all_ints.append(full_SNV_bitstring)
                 SNVs = []
 
         # encountered an INDEL
         else:
             # handle any SNVs we have collected
-            if len(SNVs) <= 10:
-                start_bitstring = shift_bit(2, 30)
-                len_SNVs = shift_bit(len(SNVs), 20)
-                SNV_bitstring = encode_SNVs(SNVs)
-                full_bitstring = start_bitstring | len_SNVs | SNV_bitstring
-                list_ints.append(full_bitstring)
-                SNVs = []
-            elif len(SNVs) > 10 and len(SNVs) <= 15:
-                list_ints.append(i for i in encode_under_fifteen_SNVs(SNVs))
-                SNVs = []
+            all_ints.append(encode_SNVs(SNVs))
+            SNVs = []
 
             # handle INDEL we encountered
             INDEL = row
             INDEL_bitstring_info = encode_INDEL(INDEL)
-            # first bitstring will need to include data about how long the INDEL is and such (for decoding)
-            head_bitstring = INDEL_bitstring_info[0]
-            tail_bitstrings = INDEL_bitstring_info[1:]
-            list_ints.append(head_bitstring)
-            for t in tail_bitstrings: list_ints.append(t)
-    # handle any SNVs left
-    if len(SNVs) > 0 and len(SNVs != 15):
-        list_ints.append(i for i in encode_under_fifteen_SNVs(SNVs))
-        SNVs = []
 
+            # # first bitstring will need to include data about how long the INDEL is and such (for decoding)
+            # head_bitstring = INDEL_bitstring_info[0]
+            # tail_bitstrings = INDEL_bitstring_info[1:]
+            # all_ints.append(head_bitstring)
+            # for t in tail_bitstrings: all_ints.append(t)
+    # handle any SNVs left
+    if len(SNVs) > 0:
+        all_ints.append(encode_SNVs(SNVs))
+        SNVs = []
     else:
         print('invalid length of SNVs')
 
-    return list_ints
+    return all_ints
 
 
 def shift_bit(bitstring, shift):
@@ -105,56 +101,64 @@ def encode_SNVs(SNVs):
     """
     given a list of SNVs, convert to int
     """
-    snv_bitstring = 0
+    SNV_bitstring = 0
+
+    # flag to specify SNVs
+    SNV_flag = shift_bit(0, 31)
+    # SNV length here is 13
+    SNV_length = shift_bit(len(SNVs), 26)
+
+    SNVs = []
     for v in range(len(SNVs)):
         # get proper bit representation for the variant
-        snv = shift_bit(get_variant_number(SNVs[v]), 2*v) | snv_bitstring
+        snv = shift_bit(get_variant_number(SNVs[v]), 2*v) | SNV_bitstring
         # if we are on first snv, set bitstring
         if v == 0:
-            snv_bitstring = snv
+            SNV_bitstring = snv
         else:
-            snv_bitstring = snv_bitstring | snv
-    return snv_bitstring
+            SNV_bitstring = SNV_bitstring | snv
 
-def encode_under_fifteen_SNVs(SNVs):
-    list_ints = []
-    if len(SNVs) <= 10:
-        start_bitstring = shift_bit(2, 30)
-        len_SNVs = shift_bit(len(SNVs), 20)
-        SNV_bitstring = encode_SNVs(SNVs)
-        full_bitstring = start_bitstring | len_SNVs | SNV_bitstring
-        list_ints.append(full_bitstring)
-    elif len(SNVs) > 10 and len(SNVs) <= 15:
-        # 1-10
-        first_ten = SNVs[0:10]
-        start_ten = shift_bit(2, 30)
-        len_ten = shift_bit(len(first_ten), 20)
-        ten_bitstring = encode_SNVs(first_ten)
-        full_ten = start_ten | len_ten | ten_bitstring
-        list_ints.append(full_ten)
-        # 10-end
-        last_snv = SNVs[10:]
-        start_last = shift_bit(2, 30)
-        len_last = shift_bit(len(last_snv), 20)
-        last_bitstring = encode_SNVs(last_snv)
-        full_last = start_last | len_last | last_bitstring
-        list_ints.append(full_last)
-    return list_ints
+    return SNV_flag | SNV_length | SNV_bitstring
 
+# def encode_under_fifteen_SNVs(SNVs):
+#     list_ints = []
+#     if len(SNVs) <= 10:
+#         start_bitstring = shift_bit(2, 30)
+#         len_SNVs = shift_bit(len(SNVs), 20)
+#         SNV_bitstring = encode_SNVs(SNVs)
+#         full_bitstring = start_bitstring | len_SNVs | SNV_bitstring
+#         list_ints.append(full_bitstring)
+#     elif len(SNVs) > 10 and len(SNVs) <= 15:
+#         # 1-10
+#         first_ten = SNVs[0:10]
+#         start_ten = shift_bit(2, 30)
+#         len_ten = shift_bit(len(first_ten), 20)
+#         ten_bitstring = encode_SNVs(first_ten)
+#         full_ten = start_ten | len_ten | ten_bitstring
+#         list_ints.append(full_ten)
+#         # 10-end
+#         last_snv = SNVs[10:]
+#         start_last = shift_bit(2, 30)
+#         len_last = shift_bit(len(last_snv), 20)
+#         last_bitstring = encode_SNVs(last_snv)
+#         full_last = start_last | len_last | last_bitstring
+#         list_ints.append(full_last)
+#     return list_ints
 
 def encode_INDEL(INDEL):
     """
-    INDELs are encoded by:
-    first 10 elements =
-    11 0000000000 00000000000000000000
-    --indel-flag = 2 bits
-    --length-full-indel = 10 bits
-    --bases = 20 bits
+    ** first int **
+    1 11          20
+    1 00000000000 00000000000000000000
+    --indels
+    --length of full indel (need this to see how many ints are used)
+    --indels
 
-    rest of elements =
-    000000 00000000000000000000000000
-    --length-of-current-segment = 6 bits
-    --bases = 26 bits
+    ** rest of ints **
+    6      26
+    000000 000000000000000000000000
+    --length of bases
+    --bases
     """
     # list of integers which encode full indel
     indel_ints = []
@@ -162,27 +166,23 @@ def encode_INDEL(INDEL):
     start_indel = 0
     end_indel = 10
 
-    # start_bitstring = shift_bit(3, 30)
-    len_INDEL = shift_bit(len(INDEL), 20)
+    len_full_INDEL = shift_bit(len(INDEL), 20)
 
-    num_ints = math.ceil(int(len(INDEL) - 10) / 16) + 1
+    num_ints = math.ceil(int(len(INDEL) - 10) / 13) + 1
     for i in range(num_ints):
         # first 10 bases are encoded differently than rest
         if i == 1:
             first_ten_bases = INDEL[start_indel:end_indel]
-            INDEL_flag = shift_bit(3, 30)
+            INDEL_flag = shift_bit(1, 31)
             first_ten_encoding = encode_SNVs(first_ten_bases)
-            indel_ints.append(INDEL_flag | len_INDEL | first_ten_encoding)
+            indel_ints.append(INDEL_flag | len_full_INDEL | first_ten_encoding)
+
         # all other bases are encoded with length in 6 bits and then room for 13 bases (26 bits)
         else:
-            curr_segment_ints = []
+            # next 13 bases
             curr_segment = INDEL[start_indel:end_indel]
-            curr_segment_length = shift_bit(len(curr_segment), 26)
-            curr_segment_encode = encode_under_fifteen_SNVs(curr_segment)
-            for s in curr_segment_encode:
-                curr_segment_ints.append(curr_segment_length | s)
-            for i in curr_segment_ints:
-                indel_ints.append(i)
+            curr_segment_encode = encode_SNVs(curr_segment)
+            indel_ints.append(curr_segment_encode)
 
         start_indel = end_indel
         end_indel += 13
--- scripts/test_ref_alt.py
+++ scripts/test_ref_alt.py
@@ -4,88 +4,39 @@ from scripts import ref_alt
 
 class TestSerializationToDeserialization(unittest.TestCase):
 
-    snpA = 'A'*15
-    snpC = 'C'*15
-    snpG = 'G'*15
-    snpT = 'T'*15
-    snp1 = 'ACG'*5
-    snp2 = 'ACT'*5
-    snp3 = 'CAT'*5
-    snp4 = 'CGG'*5
-    snp5 = 'GTA'*5
-    snp6 = 'GCC'*5
-    snp7 = 'TTA'*5
-    snp8 = 'TAG'*5
-    snp9 = 'TTAGC'*3
-    snp10 = 'GATAC'*3
-    snp11 = 'CGCAT'*3
-    snp12 = 'ACAAA'*3
-    snp13 = 'ACGTACGTACGTCCC'
-    snp14 = 'CCCCCAAAAACCCCC'
-    snp15 = 'TTTCCCGGGAAATAG'
     def test_encode_SNVs_length_15(self):
         # 00 000000000000000000000000000000
-        self.assertEqual(ref_alt.encode_SNVs(self.snpA), 0)
+        self.assertEqual(ref_alt.encode_SNVs('A'*15), 0)
         # 00 010101010101010101010101010101
-        self.assertEqual(ref_alt.encode_SNVs(self.snpC), 357913941)
+        self.assertEqual(ref_alt.encode_SNVs('C'*15), 357913941)
         # 00 101010101010101010101010101010
-        self.assertEqual(ref_alt.encode_SNVs(self.snpG), 715827882)
+        self.assertEqual(ref_alt.encode_SNVs('G'*15), 715827882)
         # 00 111111111111111111111111111111
-        self.assertEqual(ref_alt.encode_SNVs(self.snpT), 1073741823)
-        # 00 100100100100100100100100100100
-        self.assertEqual(ref_alt.encode_SNVs(self.snp1), 613566756)
-        # 00 110100110100110100110100110100
-        self.assertEqual(ref_alt.encode_SNVs(self.snp2), 886263092)
-        # 00 110001110001110001110001110001
-        self.assertEqual(ref_alt.encode_SNVs(self.snp3), 835132529)
-        # 00 101001101001101001101001101001
-        self.assertEqual(ref_alt.encode_SNVs(self.snp4), 698784361)
-        # 00 001110001110001110001110001110
-        self.assertEqual(ref_alt.encode_SNVs(self.snp5), 238609294)
-        # 00 010110010110010110010110010110
-        self.assertEqual(ref_alt.encode_SNVs(self.snp6), 374957462)
-        # 00 001111001111001111001111001111
-        self.assertEqual(ref_alt.encode_SNVs(self.snp7), 255652815)
-        # 00 100011100011100011100011100011
-        self.assertEqual(ref_alt.encode_SNVs(self.snp8), 596523235)
-        # 00 011000111101100011110110001111
-        self.assertEqual(ref_alt.encode_SNVs(self.snp9), 418790799)
-        # 00 010011001001001100100100110010
-        self.assertEqual(ref_alt.encode_SNVs(self.snp10), 321177906)
-        # 00 110001100111000110011100011001
-        self.assertEqual(ref_alt.encode_SNVs(self.snp11), 832333593)
-        # 00 000000010000000001000000000100
-        self.assertEqual(ref_alt.encode_SNVs(self.snp12), 4198404)
-        # 00 010101111001001110010011100100
-        self.assertEqual(ref_alt.encode_SNVs(self.snp13), 367322340)
-        # 00 010101010100000000000101010101
-        self.assertEqual(ref_alt.encode_SNVs(self.snp14), 357564757)
-        # 00 100011000000101010010101111111
-        self.assertEqual(ref_alt.encode_SNVs(self.snp15), 587375999)
+        self.assertEqual(ref_alt.encode_SNVs('T'*15), 1073741823)
 
-    snp16 = 'A' * 25
-    snp17 = 'C' * 25
-    snp18 = 'G' * 25
-    snp19 = 'T' * 25
-    snp20 = 'T' * 40
-    snp21 = 'C' * 11
-    snp22 = 'ACGT'*7
-    def test_encode_SNVs_greater_than_15(self):
-        # 00 000000000000000000000000000000, 10 000000101000000000000000000000
-        self.assertEqual(ref_alt.col_input(self.snp16), [0, 2157969408])
-        # 00 010101010101010101010101010101, 10 000000101001010101010101010101
-        self.assertEqual(ref_alt.col_input(self.snp17), [357913941, 2158318933])
-        # 00 101010101010101010101010101010, 10 000000101010101010101010101010
-        self.assertEqual(ref_alt.col_input(self.snp18), [715827882, 2158668458])
-        # 00 111111111111111111111111111111, 10 000000101011111111111111111111
-        self.assertEqual(ref_alt.col_input(self.snp19), [1073741823, 2159017983])
-        # 00 111111111111111111111111111111, 00 111111111111111111111111111111, 10 0000001010 11111111111111111111
-        self.assertEqual(ref_alt.col_input(self.snp20), [1073741823, 1073741823, 2159017983])
-        # 10 0000001010 01010101010101010101, 10 0000000001 00000000000000000001
-        self.assertEqual(ref_alt.col_input(self.snp21), [2158318933, 2148532225])
-        # ACGTACGTACGTACG TACGTACGTA CGT
-        # 00 100100111001001110010011100100, 10 0000001010 00111001001110010011, 10 0000000011 00000000000000111001
-        self.assertEqual(ref_alt.col_input(self.snp22), [618980580, 2158203795, 2150629433])
+    # snp16 = 'A' * 25
+    # snp17 = 'C' * 25
+    # snp18 = 'G' * 25
+    # snp19 = 'T' * 25
+    # snp20 = 'T' * 40
+    # snp21 = 'C' * 11
+    # snp22 = 'ACGT'*7
+    # def test_encode_SNVs_greater_than_15(self):
+    #     # 00 000000000000000000000000000000, 10 000000101000000000000000000000
+    #     self.assertEqual(ref_alt.col_input(self.snp16), [0, 2157969408])
+    #     # 00 010101010101010101010101010101, 10 000000101001010101010101010101
+    #     self.assertEqual(ref_alt.col_input(self.snp17), [357913941, 2158318933])
+    #     # 00 101010101010101010101010101010, 10 000000101010101010101010101010
+    #     self.assertEqual(ref_alt.col_input(self.snp18), [715827882, 2158668458])
+    #     # 00 111111111111111111111111111111, 10 000000101011111111111111111111
+    #     self.assertEqual(ref_alt.col_input(self.snp19), [1073741823, 2159017983])
+    #     # 00 111111111111111111111111111111, 00 111111111111111111111111111111, 10 0000001010 11111111111111111111
+    #     self.assertEqual(ref_alt.col_input(self.snp20), [1073741823, 1073741823, 2159017983])
+    #     # 10 0000001010 01010101010101010101, 10 0000000001 00000000000000000001
+    #     self.assertEqual(ref_alt.col_input(self.snp21), [2158318933, 2148532225])
+    #     # ACGTACGTACGTACG TACGTACGTA CGT
+    #     # 00 100100111001001110010011100100, 10 0000001010 00111001001110010011, 10 0000000011 00000000000000111001
+    #     self.assertEqual(ref_alt.col_input(self.snp22), [618980580, 2158203795, 2150629433])
 
     # indelA = ['AAAAA']*1
     # indelC = ['CCCCC']*1
