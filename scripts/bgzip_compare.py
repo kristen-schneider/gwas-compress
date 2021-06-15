d415aebc7e2f8990ea8cb7e84dcd454b1c34d583
kristen-schneider <kristen.schneider@colorado.edu>
2021-06-15 13:17:02 -0600

    a single file for bgzip compression style is added. no functionality is impleemented. just starting out and practicing with branches

--- scripts/bgzip_compare.py
+++ scripts/bgzip_compare.py
@@ -4,3 +4,17 @@ block compression, which is to take a file, split into blocks, and compress each
 """
 
 def parse_file(in_file, block_size):
+    """
+    with bgzip style compression, we parse the file into blocks and compress
+    """
+    # open file
+    gwas_f = open(in_file, 'r')
+    # close file
+    gwas_f.close()
+
+def compress_block(block):
+    """
+    will call a method to compression a single block
+    """
+    compressed_block = None
+    return compressed_block
