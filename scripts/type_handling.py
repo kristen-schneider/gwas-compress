a3dd31419ebb431f5fafd7383b1c97cfee6d90aa
kristen-schneider <kristen.schneider@colorado.edu>
2021-06-29 10:21:09 -0500

    hybrid strings basic unit tests working

--- scripts/type_handling.py
+++ scripts/type_handling.py
@@ -1,4 +1,5 @@
 import packed_strings
+import hybrid_strings
 #import ref_alt_smaller_ints
 
 def get_column_types(row, data_type_code_book):
@@ -62,7 +63,8 @@ def string_list_to_int(data_list, data_type, column_i):
     """
     typed_list = []
     if column_i == 2 or column_i == 3:
-        typed_list = packed_strings.encode_column(data_list)
+        typed_list = hybrid_strings.encode_column(data_list)
+        #typed_list = packed_strings.encode_column(data_list)
     else:
         for data in data_list:
             typed_data = convert_to_int(data, data_type)
