602ce4cb10a6cc587cb452e49dd0712e2d299454
Kristen Schneider <krsc0813@mendel.colorado.edu>
2021-07-14 14:18:56 -0600

    prints length of bitstrings, but gzip takes time for 10000 lines

--- scripts/comp_methods.py
+++ scripts/comp_methods.py
@@ -1,11 +1,14 @@
 import generate_funnel_format
+import type_handling
+import compress_column
+import serialize_body
+import packed_strings
+
 import struct
 import gzip
 import fpzip
 import zfpy
 import numpy as np
-import compress_column
-import serialize_body
 
 
 
@@ -14,40 +17,61 @@ def main():
     take in string ff data and test different
     compression methods on each column to see what is going on.
     '''
-    ff = generate_funnel_format.make_all_blocks('/home/krsc0813/projects/gwas-compress/data/in/ten.tsv',
-                                                 3, 10, '\t')
+    print('funnel format computing...')
+    ff = generate_funnel_format.make_all_blocks('/home/krsc0813/projects/gwas-compress/data/in/ten_thousand.tsv',
+                                                 10000, 10, '\t')
+    print('funnel format complete.')
     
     out_f_dir = '/home/krsc0813/projects/gwas-compress/plot_data/compression_methods/'    
 
     all_f = open(out_f_dir+'all_methods_test.tsv', 'a')
     all_f.truncate(0)
-    all_f.close
-        
+    all_f.write('gzip(int)\tgzip(string)\tfpzip\tzfpy\tfastpfor128\t')
+    all_f.write('\n')
+    
     # for each block
     for block in range(len(ff)):
-        print('BLOCK ', block+1)
+        all_f.write('BLOCK ' + str(block+1) + '\n')
         
         # for each column
         for c in range(len(ff[0])):
-            print('\tcolumn: ', fc)
-            str_column = ff[block][fc]  # column as string data
+            str_column = ff[block][c]  # column as string data
                         
-            
+            print('integer conversion...')
             int_column = []     # column as int data
-            for d in str_column:
-                int_columm.append(float_to_int(d))
-    
+            # int columns
+            if c < 2:
+                for s in str_column:
+                    int_column.append(type_handling.int_to_int(s))    
+            # float columns
+            elif c > 3 and c < 9:
+                for s in str_column:
+                    int_column.append(type_handling.float_to_int(s))
+            # true/false columns
+            elif c == 9:
+                for s in str_column:
+                    int_column.append(type_handling.string_to_int(s))        
+            # ref/alt columns
+            else:
+                int_column = packed_strings.encode_column(str_column)
+            print('integer conversion complete.')
             
-            print('gzip: ', len(gzip_copression(int_column)))
-            print('fpzip: ', len(fpzip_compression(int_column)))
-            print('zfpy: ', len(zfpy_compression(int_column)))
-            fastp_np = int_fastp_compression(int_column)
+            #fastp_np = fastp_compression(int_column)
+            all_f.write(str(len(gzip_compression(int_column, 1)))+'\t')
+            all_f.write(str(len(gzip_compression(str_column, 3)))+'\t') 
+            all_f.write(str(len(fpzip_compression(int_column)))+'\t')
+            all_f.write(str(len(zfpy_compression(int_column)))+'\t')
+            all_f.write(str(len(fastp_compression(int_column).tobytes(order='C')))+'\n')
+            #print('gzip: ', len(gzip_copression(int_column)))
+            #print('fpzip: ', len(fpzip_compression(int_column)))
+            #print('zfpy: ', len(zfpy_compression(int_column)))
             # print('fastp fpzip: ', len(fpzip_compression(fastp_np)))
             # print('fastp zfpy: ', len(zfpy_compression(fastp_np)))
             # print('int/fastpfor: ', len(int_fastp_compression(int_list)))
-            print('int to bytes: ', len(int_fastp_compression(int_list).tobytes(order='C')))
+            #print('int to bytes: ', len(int_fastp_compression(int_column).tobytes(order='C')))
         
-        print('\n')
+        all_f.write('\n')
+
     
     #str_data = ['4.213e-05', '2.984e-05', '7.127e-04']
     #data = [4.213e-05, 2.984e-05, 7.127e-04]
@@ -55,10 +79,10 @@ def main():
     #print('fpzip: ', fpzip_compression(data))
     #print('int/fastpfor: ', int_fastp_compression(str_data))
 
-def gzip_copression(data):
+def gzip_compression(data, data_type):
     serialized_bytes = b''
     for d in data:
-        serialized_bytes+=bytearray(struct.pack("f", d))
+        serialized_bytes+=serialize_body.serialize_list(data, data_type, 4)
     compressed_bytes = gzip.compress(serialized_bytes)
     return compressed_bytes
 
@@ -73,62 +97,13 @@ def zfpy_compression(data):
     compressed_bytes = zfpy.compress_numpy(np_data)
     return compressed_bytes
 
-def int_fastp_compression(data):
+def fastp_compression(data):
     #int_list = []
     #for d in data:
     #    int_list.append(float_to_int(d))
         
     compressed_data = compress_column.compress_single_column_pyfast(data, 'fastpfor128') 
     return compressed_data   
-
-def float_to_int(float_data):
-    """
-    converts a data type of float to a list of integers which can reconstruct the float
-
-    INPUT
-        float_data: data in float form (e.g. 4.213e-05)
-
-    OUTPUT
-        int_data: large integer with little endian formatting number:
-        base  exp -/+
-        00000 000 0
-    """
-    # 000000000 - little endian
-    float_as_int = 0
-    if float_data == 'NA':
-        # choose a value that is not seen in data
-        float_as_int = 999
-    else:
-        # base, base_sign, exponent, exponent_sign
-        # 00000, 0, 00, 0,
-
-        base_exponent = float_data.split('e')
-        base = float(base_exponent[0])
-        exponent = int(base_exponent[1])
-
-        # BASE
-        # base number gets proper space (e.g. 4.213 --> 42130)
-        # have to do this in two steps because
-        # rounding is lossy with python multiplication
-        # if we just did *100000000 we would get junk in the last 4 digits
-        float_as_int += abs(int(base*10000))
-        float_as_int *= 10000
-
-        # BASE SIGN
-        # is number negative or positive?
-        base_sign = 1  # positive
-        if float(base) < 0: base_sign = 0  # negative
-        float_as_int += base_sign * 1000
-
-        # EXPONENT
-        # exponents must be < 100
-        # otherwise the placement of the exponent bleeds into the base/base sign
-        float_as_int += abs(exponent) * 10
-
-        # EXPONENT SIGN
-        if exponent > 0: float_as_int += 1
-
-    return float_as_int
 
 if __name__ == '__main__': main()
 
