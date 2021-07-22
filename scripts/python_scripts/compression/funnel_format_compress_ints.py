8c30ac728b307d6c685c1d69ab8d1acf2351e893
kristen-schneider <kristen.schneider@colorado.edu>
2021-07-22 12:40:08 -0600

    modularizing

--- scripts/python_scripts/compression/funnel_format_compress_ints.py
+++ scripts/python_scripts/compression/funnel_format_compress_ints.py
@@ -1,6 +1,12 @@
 # python_scripts imports
 from datetime import datetime
 import numpy as np
+import os
+import sys
+
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
 
 # personal imports
 from scripts.python_scripts.utils import type_handling
