8ca34f7d804af24d876eec2f7227919d7f65c2f2
Kristen Schneider <krsc0813@mendel.colorado.edu>
2021-07-28 13:23:02 -0600

    changed config file for mendel

--- scripts/python_scripts/new_compression/squish.py
+++ scripts/python_scripts/new_compression/squish.py
@@ -37,9 +37,9 @@ def main():
     get_arguments_start_time = datetime.now()
 
     # user should edit config.ini to reflect proper parameters
-    args = config_arguments.get_args_from_config('LOCAL', config_file)
+    # args = config_arguments.get_args_from_config('LOCAL', config_file)
 
-    # args = config_arguments.get_args_from_config('MENDEL', config_file)#, 'fastpfor')
+    args = config_arguments.get_args_from_config('MENDEL', config_file)#, 'fastpfor')
 
     # included in config file
     IN_FILE = args['in_file']
