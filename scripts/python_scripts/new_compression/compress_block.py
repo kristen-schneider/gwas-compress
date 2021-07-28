2db034f7b7df41ce5acd4346cb78a166ac6138e2
kristen-schneider <kristen.schneider@colorado.edu>
2021-07-28 13:31:40 -0600

    returning block header and block both as bitstrings from compress_block

--- scripts/python_scripts/new_compression/compress_block.py
+++ scripts/python_scripts/new_compression/compress_block.py
@@ -1,4 +1,5 @@
 import compress_column
+import serialize_body
 
 def compress_block(block, codecs_list, column_types, input_data_type_list, data_type_byte_sizes):
     # serialized block info:
@@ -6,7 +7,11 @@ def compress_block(block, codecs_list, column_types, input_data_type_list, data_
     ##  serliazed block data
     serialized_block = b''
    
-    block_header_compression_method = 'gzip'
+    block_header_codec = 'gzip'
+
+    block_header_list = []
+    block_header_bitstring = b''
+    block_bitstring = b''
     
     # compress one column at a time
     for column_i in range(len(block)):
@@ -19,5 +24,11 @@ def compress_block(block, codecs_list, column_types, input_data_type_list, data_
 
         compressed_column_bitstring = compress_column.column_compression_main(curr_column, curr_codec, curr_data_type,
                                                 curr_compression_data_type, curr_data_type_byte_sizes)
-        print(compressed_column_bitstring)
+        block_header_list.append(len(compressed_column_bitstring))
+        block_bitstring += compressed_column_bitstring
+
+    block_header_bitstring = compress_column.column_compression_main(block_header_list, block_header_codec,
+                                                                     1, 1, data_type_byte_sizes[1])
+
+    return block_header_bitstring, block_bitstring
 
