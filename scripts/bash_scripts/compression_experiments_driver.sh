9cc46e50bb06d2ae600e695b54eec6aa1d7005ae
Kristen Schneider <krsc0813@mendel.colorado.edu>
2021-07-30 16:04:04 -0600

    changing directory places for output data

--- scripts/bash_scripts/compression_experiments_driver.sh
+++ scripts/bash_scripts/compression_experiments_driver.sh
@@ -26,30 +26,3 @@ do
         #basic_config
     fi
 done        
-#for method in "${comp_methods[@]}"
-#do
-#    echo $method
-#    config_file="${method}_config.ini"
-#    python_scripts driver.py
-#    echo
-#done
-
-#do
-#    if [[ $pileup_file == *.bed* ]] && [[ $pileup_file != *.tbi ]]; then
-#        echo $pileup_file
-#
-#        # prepare the name of the output file by removing the pattern '.*' greedily
-#        sample=${pileup_file%%.*}
-#        out_file=${out_dir}${sample}
-#
-#        # Check if the output file already exists, if so, skip the conversion
-#        if test -e $out_file; then
-#            echo "$out_file already exists, so we skip processing it"
-#        else
-#            echo "submitting a job for" $sample
-#            sbatch quality_worker.sh $sample $pileups$pileup_file $regions $out_dir
-#        fi
-#    #else
-#        #echo "not using" $pileup_file
-#    fi
-#done
