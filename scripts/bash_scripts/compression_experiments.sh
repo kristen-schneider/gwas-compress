9e129f20e927900ba10a0b97f613daf3eb6cb27a
kristen-schneider <kristen.schneider@colorado.edu>
2021-08-02 14:24:49 -0600

    changing params for bash files so a wrapper can run all

--- scripts/bash_scripts/compression_experiments.sh
+++ scripts/bash_scripts/compression_experiments.sh
@@ -10,15 +10,22 @@
 
 # PURPOSE: run multiple experiments for different setups of the compression
 
-in_file='/home/krsc0813/projects/gwas-compress/gwas_files/in/hundred.tsv'
-scripts_dir='/home/krsc0813/projects/gwas-compress/scripts/python_scripts/new_compression/'
-out_dir='/home/krsc0813/projects/gwas-compress/plot_data/'
-config_files_dir='/home/krsc0813/projects/gwas-compress/config_files/'
-basic_config='config.ini' 
-declare -a comp_methods=("bz2" "fastpfor" "fpzip" "gzip" "zfpy" "zlib")
-#declare -a block_size=
-block_size=3
-declare -a input_data_type=(1,1,1,1,1,1,1,1,1,1)
+# 1. sample name
+in_file=$1
+config_file=$2
+block_size=$3
+input_data_type=$4
+out_file=$5
+
+#in_file='/home/krsc0813/projects/gwas-compress/gwas_files/in/hundred.tsv'
+#scripts_dir='/home/krsc0813/projects/gwas-compress/scripts/python_scripts/new_compression/'
+#out_dir='/home/krsc0813/projects/gwas-compress/plot_data/'
+#config_files_dir='/home/krsc0813/projects/gwas-compress/config_files/'
+#basic_config='config.ini'
+#declare -a comp_methods=("bz2" "fastpfor" "fpzip" "gzip" "zfpy" "zlib")
+##declare -a block_size=
+#block_size=3
+#declare -a input_data_type=(1,1,1,1,1,1,1,1,1,1)
 
 echo "starting experiments calculation"
 for config_file in `ls $config_files_dir`
@@ -26,12 +33,6 @@ do
     config_root=${config_file%%_*}
     if [[ $config_file == *.ini ]] && [[ $config_file != $basic_config ]];then
         echo "running experiments for $config_file"
-        python $scripts_dir"driver.py" \
-          $in_file \
-          $config_files_dir$config_file \
-          $block_size \
-          $input_data_type \
-          > $out_dir/out/$config_root.out
-        #basic_config
+        python $scripts_dir"driver.py" $1 $2 $3 $4 > $5
     fi
 done        
--- scripts/bash_scripts/run_all_compression.sh
+++ scripts/bash_scripts/run_all_compression.sh
@@ -10,11 +10,19 @@
 
 # PURPOSE: run multiple experiments for different setups of the compression
 
+
 in_file='/home/krsc0813/projects/gwas-compress/gwas_files/in/hundred_thousand.tsv'
-scripts_dir='/home/krsc0813/projects/gwas-compress/scripts/python_scripts/new_compression/'
-out_dir='/home/krsc0813/projects/gwas-compress/plot_data/'
+
+python_scripts_dir='/home/krsc0813/projects/gwas-compress/scripts/python_scripts/'
+bash_scripts_dir='/home/krsc0813/projects/gwas-compress/scripts/bash_scripts'
+
 config_files_dir='/home/krsc0813/projects/gwas-compress/config_files/'
-basic_config='config.ini' 
+
+out_dir='/home/krsc0813/projects/gwas-compress/plot_data/out/'
+ratios_dir='/home/krsc0813/projects/gwas-compress/plot_data/ratios/'
+times_dir='/home/krsc0813/projects/gwas-compress/plot_data/times/'
+
+basic_config='config.ini'
 declare -a comp_methods=("bz2" "fastpfor" "fpzip" "gzip" "zfpy" "zlib")
 #declare -a block_size=
 block_size=10000
@@ -25,25 +33,32 @@ for config_file in `ls $config_files_dir`
 do
     config_root=${config_file%%_*}
     # 1. do compression and write to .out files
-    
+    bash $bash_scripts_dir"compression_experiments.sh" \
+      $in_file \
+      $config_files_dir$config_file \
+      $block_size \
+      $input_data_type \
+      $out_dir$config_root.out
+
     # 2. split .out files in to .ratio and .time intermediate files
+
     # 3. plot boxplots for intermediate files
 
 done
 
-
-echo "starting experiments calculation"
-for config_file in `ls $config_files_dir`
-do
-    config_root=${config_file%%_*}
-    if [[ $config_file == *.ini ]] && [[ $config_file != $basic_config ]];then
-        echo "running experiments for $config_file"
-        python $scripts_dir"driver.py" \
-          $in_file \
-          $config_files_dir$config_file \
-          $block_size \
-          $input_data_type \
-          > $out_dir/out/$config_root.out
-        #basic_config
-    fi
-done        
+#
+#echo "starting experiments calculation"
+#for config_file in `ls $config_files_dir`
+#do
+#    config_root=${config_file%%_*}
+#    if [[ $config_file == *.ini ]] && [[ $config_file != $basic_config ]];then
+#        echo "running experiments for $config_file"
+#        python $python_scripts_dir"driver.py" \
+#          $in_file \
+#          $config_files_dir$config_file \
+#          $block_size \
+#          $input_data_type \
+#          > $out_dir/out/$config_root.out
+#        #basic_config
+#    fi
+#done
