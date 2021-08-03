9e1fe30749709863bdcd261755282eee377365d0
Kristen Schneider <krsc0813@mendel.colorado.edu>
2021-08-03 08:12:33 -0600

    one bash script to compress, write, and plot

--- scripts/bash_scripts/run_all_compression.sh
+++ scripts/bash_scripts/run_all_compression.sh
@@ -18,14 +18,13 @@ bash_scripts_dir='/home/krsc0813/projects/gwas-compress/scripts/bash_scripts/'
 config_files_dir='/home/krsc0813/projects/gwas-compress/config_files/'
 
 out_dir='/home/krsc0813/projects/gwas-compress/plot_data/out/'
-ratios_dir='/home/krsc0813/projects/gwas-compress/plot_data/ratios/'
-times_dir='/home/krsc0813/projects/gwas-compress/plot_data/times/'
+ratios_intermediate_dir='/home/krsc0813/projects/gwas-compress/plot_data/ratios/'
+times_intermediate_dir='/home/krsc0813/projects/gwas-compress/plot_data/times/'
 
 plots_dir='/home/krsc0813/projects/gwas-compress/plots/'
 
 basic_config='config.ini'
 declare -a comp_methods=("bz2" "fastpfor" "fpzip" "gzip" "zfpy" "zlib")
-#declare -a block_size=
 block_size=10000
 declare -a input_data_type=(1,1,1,1,1,1,1,1,1,1)
 
@@ -35,59 +34,49 @@ echo ""
 # 1. do compression and write to .out files
 for config_file in `ls $config_files_dir`
 do
-    config_root=${config_file%%_*}
+    experiment_name=${config_file%%_*}
     if [[ $config_file == *.ini ]] && [[ $config_file != $basic_config ]]; then
       echo "running experiments for $config_file"
-      python $python_scripts"/new_compression/driver.py" \
+      python $python_scripts_dir"/new_compression/driver.py" \
         $in_file \
         $config_files_dir$config_file \
         $block_size \
-        $input_data_type >
-        $out_dir$config_root.out
-#        bash $bash_scripts_dir"compression_experiments.sh" \
-#            $python_scripts_dir \
-#            $in_file \
-#            $config_files_dir$config_file \
-#            $block_size \
-#            $input_data_type \
-#            $out_dir$config_root.out
+        $input_data_type \
+        > $out_dir$experiment_name.out
     fi
 done
 
 # 2. split data from .out files to .ratios and .times files
 for out_file in `ls $out_dir`
 do
-    config_base_name=${out_file%%.*}
+    experiment_name=${out_file%%.*}
     if [[ $out_file == *.out ]]; then
         echo "making intermediate files for $out_file"
-        grep "ratio" $out_dir$out_file | awk '{print $1" "$3}' > $ratios_dir$config_base_name".ratios"
-        grep "time" $out_dir$out_file | awk '{print $1" "$3}' > $times_dir$config_base_name".times"
+        grep "ratio" $out_dir$out_file | awk '{print $1" "$3}' > $ratios_intermediate_dir$experiment_name".ratios"
+        grep "time" $out_dir$out_file | awk '{print $1" "$3}' > $times_intermediate_dir$experiment_name".times"
     fi
 done
 
-# 3. plot from .ratios and .times files
-for ratios_data_file in `ls $ratios_dir`
+# 3. plot from .ratios files
+for ratios_data_file in `ls $ratios_intermediate_dir`
 do
-    config_root=${ratios_data_file%%.*}
+    experiment_name=${ratios_data_file%%.*}
     if [[ $ratios_data_file == *.ratios ]] && [[ $ratios_data_file != $basic_config ]];then
         echo "plotting ratios for $ratios_data_file"
-        python $python_scripts_dir"plotting/plot_ratios.py" $ratios_dir$ratios_data_file $plots_dir"/ratios/"$config_root".png"
+        python $python_scripts_dir"plotting/plot_ratios.py" \
+            $ratios_intermediate_dir$ratios_data_file \
+            $plots_dir"/ratios/"$experiment_name".png"
     fi
 done
 
-#
-#echo "starting experiments calculation"
-#for config_file in `ls $config_files_dir`
-#do
-#    config_root=${config_file%%_*}
-#    if [[ $config_file == *.ini ]] && [[ $config_file != $basic_config ]];then
-#        echo "running experiments for $config_file"
-#        python $python_scripts_dir"driver.py" \
-#          $in_file \
-#          $config_files_dir$config_file \
-#          $block_size \
-#          $input_data_type \
-#          > $out_dir/out/$config_root.out
-#        #basic_config
-#    fi
-#done
+# 4. plot from .times files
+for times_data_file in `ls $times_intermediate_dir`
+do
+    experiment_name=${times_data_file%%.*}
+    if [[ $times_data_file == *.times ]] && [[ $times_data_file != $basic_config ]];then
+        echo "plotting times for $times_data_file"
+        python $python_scripts_dir"plotting/plot_times.py" \
+            $times_intermediate_dir$times_data_file \
+            $plots_dir"/times/"$experiment_name".png"
+    fi 
+done
