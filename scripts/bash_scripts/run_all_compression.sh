5ee6bf262bbc7286c325b9419f62bbc8f5836fcc
Kristen Schneider <krsc0813@mendel.colorado.edu>
2021-08-03 10:48:53 -0600

    run compression can make directories for new experimetns.

--- scripts/bash_scripts/run_all_compression.sh
+++ scripts/bash_scripts/run_all_compression.sh
@@ -26,7 +26,7 @@ plots_dir='/home/krsc0813/projects/gwas-compress/plots/'
 basic_config='config.ini'
 declare -a comp_methods=("bz2" "fastpfor" "fpzip" "gzip" "zfpy" "zlib")
 declare -a block_size_list=(5000,10000,15000,20000,25000,30000,35000,40000,45000,50000,60000,70000,80000,90000)
-block_size=10000
+block_size=5000
 declare -a input_data_type=(1,1,1,1,1,1,1,1,1,1)
 
 echo "** starting all experiments **"
@@ -35,47 +35,67 @@ echo ""
 # 1. do compression and write to .out files
 for config_file in `ls $config_files_dir`
 do
+    # each experiment is a block size
+    # all out files should be containted in a sub directory named by block size
+    if [[ ! -d $out_dir$block_size ]]; then
+        mkdir $out_dir$block_size
+    fi
+    experiment_dir=$out_dir$block_size"/"
     experiment_name=${config_file%%_*}$block_size
+    
     if [[ $config_file == *.ini ]] && [[ $config_file != $basic_config ]]; then
-      echo "running experiments for $config_file"
-
-      # each experiment is a block size
-      if [[ ! -d $out_dir$block_size ]]; then
-        echo $out_dir$block_size "does not exist"
-
-      python $python_scripts_dir"/new_compression/driver.py" \
-        $in_file \
-        $config_files_dir$config_file \
-        $block_size \
-        $input_data_type \
-        > $out_dir$experiment_name".out"
+        echo "running experiments for $config_file"
+        # run compression script
+        python $python_scripts_dir"/new_compression/driver.py" \
+            $in_file \
+            $config_files_dir$config_file \
+            $block_size \
+            $input_data_type \
+            > $experiment_dir$experiment_name".out"
     fi
 done
 
 # 2. split data from .out files to .ratios and .times files
-for out_file in `ls $out_dir`
+for out_file in `ls $out_dir$block_size"/"`
 do
+    # each experiment is a block size
+    # all out files should be containted in a sub directory named by block size
+    if [[ ! -d $ratios_intermediate_dir$block_size ]]; then
+        mkdir $ratios_intermediate_dir$block_size
+    fi
+    experiment_dir=$ratios_intermediate_dir$block_size"/"
     experiment_name=${out_file%%.*}
+    
     if [[ $out_file == *$block_size".out" ]]; then
         echo "making intermediate files for $out_file"
+        
+        # making intermediate_files
+        grep "ratio" $out_dir$block_size"/"$out_file | awk '{print $1" "$3}' \
+        > $experiment_dir$experiment_name".ratios"
 
-        grep "ratio" $out_dir$out_file | awk '{print $1" "$3}' \
-        > $ratios_intermediate_dir$experiment_name".ratios"
-
-        grep "time" $out_dir$out_file | awk '{print $1" "$3}' \
-        > $times_intermediate_dir$experiment_name".times"
+        grep "time" $out_dir$block_size"/"$out_file | awk '{print $1" "$3}' \
+        > $experiment_dir$experiment_name".times"
     fi
 done
 
 # 3. plot from .ratios files
-for ratios_data_file in `ls $ratios_intermediate_dir`
+for ratios_data_file in `ls $ratios_intermediate_dir$block_size`
 do
+    # each experiment is a block size
+    # all out files should be containted in a sub directory named by block size
+    if [[ ! -d $plots_dir"/ratios/"$block_size ]]; then
+        mkdir $plots_dir"/ratios/"$block_size
+    fi
+    experiment_dir=$plots_dir"/ratios/"$block_size"/"
     experiment_name=${ratios_data_file%%.*}
+    
     if [[ $ratios_data_file == *$block_size".ratios" ]] && [[ $ratios_data_file != $basic_config$block_size ]];then
         echo "plotting ratios for $ratios_data_file"
+        
+        # running plotting script on full directory
         python $python_scripts_dir"plotting/plot_ratios.py" \
-            $ratios_intermediate_dir$ratios_data_file \
-            $plots_dir"/ratios/"$experiment_name".png"
+            $ratios_intermediate_dir$block_size"/"$ratios_data_file \
+            $experiment_dir$experiment_name".png"
     fi
 done
 
