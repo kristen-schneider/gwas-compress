39f5fc9a8067154d3b8b0b41f8de3c7357cc99dc
kristen-schneider <kristen.schneider@colorado.edu>
2021-09-01 09:55:05 -0600

    adding unittest template

---
+++ scripts/python_scripts/unittests/test_template.py
@@ -0,0 +1,11 @@
+import unittest
+import numpy as np
+
+class TestCodecs(unittest.TestCase):
+    VAR1 = 0
+
+    def test_function(self):
+            self.assertEqual(function(), self.VAR1)
+
+if __name__ == '__main__':
+    unittest.main()
